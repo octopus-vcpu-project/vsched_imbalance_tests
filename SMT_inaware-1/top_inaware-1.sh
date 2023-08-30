8d18a07486a73eca8dd3dbbb122e902977f756be
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-08-30 15:06:43 -0400

    Update top_inaware-1.sh

--- SMT_inaware-1/top_inaware-1.sh
+++ SMT_inaware-1/top_inaware-1.sh
@@ -1,6 +1,6 @@
 prob_vm = $1
-benchmark_command = "sysbench --threads=16 --time=100 cpu run"
-../utility/cleanon_startup.sh prob_vm 32
+benchmark_command = "sysbench --threads=16 --time=5 cpu run"
+sudo bash ../utility/cleanon_startup.sh prob_vm 32
 
 for i in {0..15};do
     sudo virsh vcpupin $i $i
@@ -10,12 +10,12 @@ for i in {16..31};do
     sudo virsh vcpupin $i $((i + 64))
 done
 
-ssh ubuntu@$prob_vm "sudo killall sysbench" &
-
-#topology naive testing
-OUTPUT_FILE="top_inaware_naive$(date +%m%d%H%M).txt"
-ssh ubuntu@$prob_vm "sudo $benchmark_command" > "$OUTPUT_FILE"
-
-#topology smart testing
-OUTPUT_FILE="top_inaware_smart$(date +%m%d%H%M).txt"
-ssh ubuntu@$prob_vm "sudo taskset -c 0-15 $benchmark_command" > "$OUTPUT_FILE"
+    for i in {0..41};do {
+    ssh ubuntu@$prob_vm "sudo killall sysbench" &
+    OUTPUT_FILE="top_inaware_naive$(date +%m%d%H%M).txt"
+    ssh ubuntu@$prob_vm "cat sys/kernel/debug/sched | grep -E 'cpu#|>R '" > "$OUTPUT_FILE"
+    sleep 1
+    ssh ubuntu@$prob_vm "cat sys/kernel/debug/sched | grep -E 'cpu#|>R '" > "$OUTPUT_FILE"
+    #topology naive testing
+    ssh ubuntu@$prob_vm "sudo $benchmark_command" > "$OUTPUT_FILE"
+}

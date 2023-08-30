bba936d22defe99b4d5378d90d5d190de9d1c477
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-08-30 16:24:26 -0400

    Update cpc_inaware.sh

--- cpc_aware/cpc_inaware.sh
+++ cpc_aware/cpc_inaware.sh
@@ -18,5 +18,5 @@ ssh ubuntu@$compete_vm "sudo $compete_benchmark" &
 OUTPUT_FILE="./test/cpc_inaware$(date +%m%d%H%M).txt"
 #ssh ubuntu@$prob_vm "sudo $cpu_benchmark" > "$OUTPUT_FILE" &
 touch $OUTPUT_FILE
-ssh ubuntu@$prob_vm "sudo runqlen -c -T 1 60" > "$OUTPUT_FILE"
+ssh ubuntu@$prob_vm "sudo runqlen -C -T 1 60" > "$OUTPUT_FILE"
 echo "test complete"

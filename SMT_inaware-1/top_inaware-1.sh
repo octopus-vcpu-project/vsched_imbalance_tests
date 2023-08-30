91faff2eac17aaa92d5b969f1deb5923b8bde59b
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-08-30 18:23:56 -0400

    Update top_inaware-1.sh

--- SMT_inaware-1/top_inaware-1.sh
+++ SMT_inaware-1/top_inaware-1.sh
@@ -12,13 +12,15 @@ done
 
 OUTPUT_FILE="top_inaware_placement$(date +%m%d%H%M).txt"
 
-for i in {0..10};do 
+for i in {0..40};do 
     ssh ubuntu@$prob_vm "sudo killall sysbench" &
     ssh ubuntu@$prob_vm "sudo $benchmark_command" &
     sleep 1
     ssh ubuntu@$prob_vm "sudo cat /sys/kernel/debug/sched/debug | grep -E 'cpu#|>R '" >> "$OUTPUT_FILE"
+    echo "/n linebreak /n" >> $OUTPUT_FILE
     sleep 1
     ssh ubuntu@$prob_vm "sudo cat /sys/kernel/debug/sched/debug | grep -E 'cpu#|>R '" >> "$OUTPUT_FILE"
+    echo "/n linebreak /n" >> $OUTPUT_FILE
     #topology naive testing
     sleep 3
 done

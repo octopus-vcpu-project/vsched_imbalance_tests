8f171b86ca184fcab06f6bae62ab762f1f2ee4a7
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-08-30 16:12:55 -0400

    new tests

---
+++ cpc_aware/cpc_inaware.sh
@@ -0,0 +1,22 @@
+prob_vm=$1
+compete_vm=$2
+cpu_benchmark="sysbench --threads=8 --time=60 cpu run"
+compete_benchmark="sysbench --threads=16 --time=10000 cpu run"
+sudo bash ../utility/cleanon_startup.sh $prob_vm 16 $compete_vm 16
+
+for i in {0..15};do
+    sudo virsh vcpupin $prob_vm $i $i
+    sudo virsh vcpupin $compete_vm $i $i
+done
+
+echo "Finished Pinning"
+
+ssh ubuntu@$prob_vm "sudo killall sysbench" 
+ssh ubuntu@$compete_vm "sudo killall sysbench"
+ssh ubuntu@$compete_vm "sudo $compete_benchmark" 
+#topology naive testing
+OUTPUT_FILE="../tests/cpc_inaware$(date +%m%d%H%M).txt"
+#ssh ubuntu@$prob_vm "sudo $cpu_benchmark" > "$OUTPUT_FILE" &
+for i in{0..31};do
+    ssh ubuntu@$prob_vm "sudo runqlen.py -c -T 1 60" > "$OUTPUT_FILE"
+done

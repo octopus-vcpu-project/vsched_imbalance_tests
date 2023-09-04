690090100c14797d5785c218e4a3868f043f5636
FarWrong <55039915+FarWrong@users.noreply.github.com>
2023-09-04 17:10:25 -0400

    Update sym-plc.sh

--- cpc_aware/sym-plc.sh
+++ cpc_aware/sym-plc.sh
@@ -1,6 +1,6 @@
 prob_vm=$1
 compete_vm=$2
-cpu_benchmark="sysbench --threads=16 --time=60 cpu run"
+cpu_benchmark="sysbench --threads=8 --time=1000 cpu run"
 compete_benchmark="sysbench --threads=16 --time=1000 cpu run"
 sudo bash ../utility/cleanon_startup.sh $prob_vm 16 $compete_vm 16
 
@@ -14,6 +14,7 @@ echo "Finished Pinning"
 ssh ubuntu@$prob_vm "sudo killall sysbench" 
 ssh ubuntu@$compete_vm "sudo killall sysbench"
 ssh ubuntu@$compete_vm "sudo $compete_benchmark" &
+ssh ubuntu@$prob_vm "sudo $cpu_benchmark" &
 #topology naive testing
 OUTPUT_FILE="./test/sym-plc$(date +%m%d%H%M).txt"
 for i in {0..500};do

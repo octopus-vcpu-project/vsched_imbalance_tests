c527755c7c1f7209ee65c9b6f5307957262b5bde
edward <eguo3@pride.hofstra.edu>
2023-09-04 20:26:39 -0400

    all

--- cpc_aware/sym-test-optimal.sh
+++ cpc_aware/sym-test-optimal.sh
@@ -1,7 +1,7 @@
 prob_vm=$1
 compete_vm=$2
 cpu_benchmark="taskset -c 7-15 sysbench --threads=8 --time=3 cpu run"
-compete_benchmark="sysbench --threads=16 --time=1000 cpu run"
+compete_benchmark="sysbench --threads=16 --time=50000 cpu run"
 sudo bash ../utility/cleanon_startup.sh $prob_vm 16 $compete_vm 16
 
 for i in {0..15};do
@@ -17,7 +17,7 @@ ssh ubuntu@$compete_vm "sudo $compete_benchmark" &
 
 #topology naive testing
 OUTPUT_FILE="./test/sym-naive$(date +%m%d%H%M).txt"
-for i in {0..500};do
+for i in {0..100};do
     ssh ubuntu@$prob_vm "sudo $cpu_benchmark" >> "$OUTPUT_FILE"
 done
 touch $OUTPUT_FILE

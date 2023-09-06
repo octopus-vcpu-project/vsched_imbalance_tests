e004ef2aa825378c15d145120bd9e17f87f9646a
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-09-06 19:14:08 -0400

    asdf

---
+++ cpc_aware/mismatch-hdwr-pm.sh
@@ -0,0 +1,19 @@
+cpu_benchmark="sysbench --threads=4 --report-interval=3 --time=50 cpu run"
+
+for i in {0..7};do
+    sudo virsh vcpupin $prob_vm $i $((i+16))
+done
+
+taskset -c 0-15 sysbench --threads=16 --time=100000 cpu run &
+taskset -c 80-95 sysbench --threads=16 --time=100000 cpu run &
+echo "Finished Pinning/compeition"
+
+ssh ubuntu@$prob_vm "sudo killall sysbench" 
+
+#topology naive testing
+OUTPUT_FILE1="./test/1-dis-hrd-pm$(date +%m%d%H%M).txt"
+OUTPUT_FILE2="./test/2-dis-hrd-pm$(date +%m%d%H%M).txt"
+sudo taskset -c 16-19 sysbench --threads=4 --report-interval=3 --time=50 cpu run >> $OUTPUT_FILE1 &
+sudo taskset -c 20-23 sysbench --threads=4 --report-interval=3 --time=50 cpu run >> $OUTPUT_FILE2 
+touch $OUTPUT_FILE
+echo "test complete"

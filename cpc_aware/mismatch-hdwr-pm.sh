e00e05a9918063e388dfe7da7312cd9cd1e2fe28
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-09-06 20:01:05 -0400

    Update mismatch-hdwr-pm.sh

--- cpc_aware/mismatch-hdwr-pm.sh
+++ cpc_aware/mismatch-hdwr-pm.sh
@@ -1,9 +1,5 @@
 cpu_benchmark="sysbench --threads=4 --report-interval=3 --time=50 cpu run"
 
-for i in {0..7};do
-    sudo virsh vcpupin $prob_vm $i $((i+16))
-done
-
 taskset -c 0-15 sysbench --threads=16 --time=100000 cpu run &
 taskset -c 80-95 sysbench --threads=16 --time=100000 cpu run &
 echo "Finished Pinning/compeition"

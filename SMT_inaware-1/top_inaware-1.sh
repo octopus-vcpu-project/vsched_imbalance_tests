fd590041945f1375eb97480282f57a36bd22bcef
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-09-08 14:42:09 -0400

    asdf

--- SMT_inaware-1/top_inaware-1.sh
+++ SMT_inaware-1/top_inaware-1.sh
@@ -1,6 +1,6 @@
 
 prob_vm=$1
-benchmark_command="sysbench --threads=16 --time=5 cpu run"
+benchmark_command="sysbench --threads=16 --time=10000 cpu run"
 sudo bash ../utility/cleanon_startup.sh $prob_vm 32
 
 for i in {0..15};do
@@ -12,16 +12,11 @@ for i in {16..31};do
 done
 
 OUTPUT_FILE="top_inaware_placement$(date +%m%d%H%M).txt"
-
-for i in {0..40};do 
-    ssh ubuntu@$prob_vm "sudo killall sysbench" &
-    ssh ubuntu@$prob_vm "sudo $benchmark_command" &
-    sleep 1
+ssh ubuntu@$prob_vm "sudo killall sysbench" &
+ssh ubuntu@$prob_vm "sudo $benchmark_command" &
+for i in {0..500};do 
+    sleep 0.5
     ssh ubuntu@$prob_vm "sudo cat /sys/kernel/debug/sched/debug | grep -E 'cpu#|>R '" >> "$OUTPUT_FILE"
-    echo "/n linebreak /n" >> $OUTPUT_FILE
-    sleep 1
-    ssh ubuntu@$prob_vm "sudo cat /sys/kernel/debug/sched/debug | grep -E 'cpu#|>R '" >> "$OUTPUT_FILE"
-    echo "/n linebreak /n" >> $OUTPUT_FILE
-    #topology naive testing
-    sleep 3
 done
+
+echo "done"

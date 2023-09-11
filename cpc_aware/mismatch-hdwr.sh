2ffcf826ef07273661da7bd6bde6584556a7f6be
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-09-11 19:23:07 -0400

    Update mismatch-hdwr.sh

--- cpc_aware/mismatch-hdwr.sh
+++ cpc_aware/mismatch-hdwr.sh
@@ -16,12 +16,13 @@ ssh ubuntu@$prob_vm "sudo killall sysbench"
 #topology naive testing
 OUTPUT_FILE1="./test/newtest$(date +%m%d%H%M).txt"
 OUTPUT_FILE2="./test/newdtest$(date +%m%d%H%M).txt"
-for j in {0...30};do 
+for i in {0...30};do 
     ssh ubuntu@$prob_vm "sudo $cpu_benchmark" >> "$OUTPUT_FILE1" &
     ssh ubuntu@$prob_vm "sudo $cpu_benchmark" >> "$OUTPUT_FILE2"
     sleep 1
     echo "linebreak" >> $OUTPUT_FILE1
     echo "linebreak" >> $OUTPUT_FILE2
+    echo i
 done
 touch $OUTPUT_FILE
 echo "test complete"

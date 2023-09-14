91b061b1b1011cfa50f536416442bcb3d38543c6
FarWrong <55039915+FarWrong@users.noreply.github.com>
2023-09-13 22:19:30 -0400

    Update thread-unfairness.sh

--- cpc_aware/thread-unfairness.sh
+++ cpc_aware/thread-unfairness.sh
@@ -70,7 +70,6 @@ declare -a thread_ids
 for tid in $(ssh ubuntu@$prob_vm "ls /proc/$sysbench_pid/task");do
     thread_ids+=($tid)
 done
-pin_threads_smartly "${thread_ids[@]}"
 
 
 for i in {0..20};do

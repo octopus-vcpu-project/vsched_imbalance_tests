aab6282cc84d179e794c69070937d7aa7f6bdcd0
FarWrong <55039915+FarWrong@users.noreply.github.com>
2023-09-13 22:49:54 -0400

    asdf

--- cpc_aware/parse-unfair.py
+++ cpc_aware/parse-unfair.py
@@ -51,7 +51,8 @@ print(biggest_element)
 array_of_lasts.sort()
 print(array_of_lasts)
 for k, v in vruntime_per_thread.items():
-    plt.plot(range(1, len(v) + 1), v, '.-', label=k)
+    if(v[-1] > array_of_lasts[-5] or v[-1] < array_of_lasts[4]):
+        plt.plot(range(1, len(v) + 1), v, '.-', label=k)
 plt.legend()  # To draw legend
 plt.show()
 

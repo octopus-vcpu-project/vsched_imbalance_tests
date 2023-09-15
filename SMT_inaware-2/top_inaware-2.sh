38543ae2a4f134b9b32ad5cae8733fe12dec475f
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-09-15 14:56:16 -0400

    Update top_inaware-2.sh

--- SMT_inaware-2/top_inaware-2.sh
+++ SMT_inaware-2/top_inaware-2.sh
@@ -16,11 +16,11 @@ ssh ubuntu@$prob_vm "sudo killall sysbench" &
 #topology naive testing
 OUTPUT_FILE="top_inaware_2_cpu_naive$(date +%m%d%H%M).txt"
 ssh ubuntu@$prob_vm "sudo $cpu_benchmark" > "$OUTPUT_FILE" &
-OUTPUT_FILE="top_inaware_2_io_naive$(date +%m%d%H%M).txt"
-ssh ubuntu@$prob_vm "sudo $io_benchmark" > "$OUTPUT_FILE"
-sleep 3
+OUTPUT_FILE1="top_inaware_2_io_naive$(date +%m%d%H%M).txt"
+ssh ubuntu@$prob_vm "sudo $io_benchmark" > "$OUTPUT_FILE1"
+sleep 10
 #topology smart testing
-OUTPUT_FILE="top_inaware_2_cpu_smart$(date +%m%d%H%M).txt"
-ssh ubuntu@$prob_vm "sudo taskset -c 0-15 $cpu_benchmark" > "$OUTPUT_FILE" &
-OUTPUT_FILE="top_inaware_2_io_smart$(date +%m%d%H%M).txt"
-ssh ubuntu@$prob_vm "sudo taskset -c 16-31 $io_benchmark" > "$OUTPUT_FILE"
+OUTPUT_FILE2="top_inaware_2_cpu_smart$(date +%m%d%H%M).txt"
+ssh ubuntu@$prob_vm "sudo taskset -c 0-15 $cpu_benchmark" > "$OUTPUT_FILE2" &
+OUTPUT_FILE3="top_inaware_2_io_smart$(date +%m%d%H%M).txt"
+ssh ubuntu@$prob_vm "sudo taskset -c 16-31 $io_benchmark" > "$OUTPUT_FILE3"

ae911ee85fb7d152ab8e01b6f17378df03c84f9e
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-09-15 15:14:09 -0400

    Update top_inaware-2.sh

--- SMT_inaware-2/top_inaware-2.sh
+++ SMT_inaware-2/top_inaware-2.sh
@@ -1,6 +1,6 @@
 prob_vm=$1
-cpu_benchmark="sysbench --threads=16 --time=100 cpu run"
-io_benchmark="sysbench --file-test-mode=rndrw --threads=16 --file-total-size=10G --max-time=100 fileio run"
+cpu_benchmark="sysbench --threads=16 --time=50 cpu run"
+io_benchmark="sysbench --file-test-mode=rndrw --threads=16 --file-total-size=10G --max-time=50 fileio run"
 sudo bash ../utility/cleanon_startup.sh $prob_vm 32
 
 for i in {0..15};do
@@ -12,11 +12,11 @@ for i in {16..31};do
 done
 
 ssh ubuntu@$prob_vm "sudo killall sysbench" 
-
+ssh ubuntu@$prob_vm "sudo sysbench --threads=16 --time=20 cpu run" 
 #topology naive testing
 OUTPUT_FILE="top_inaware_2_cpu_naive$(date +%m%d%H%M).txt"
 ssh ubuntu@$prob_vm "sudo $cpu_benchmark;sudo $io_benchmark" >> "$OUTPUT_FILE" 
-sleep 10
+sleep 2
 #topology smart testing
 OUTPUT_FILE2="top_inaware_2_cpu_smart$(date +%m%d%H%M).txt"
 ssh ubuntu@$prob_vm "sudo taskset -c 0-15 $cpu_benchmark;sudo taskset -c 16-31 $io_benchmark" >> "$OUTPUT_FILE2" 

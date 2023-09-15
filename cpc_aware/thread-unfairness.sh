8860e75fe3def02bb7f0033cf5460b7756c5c73f
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-09-15 17:47:59 -0400

    Update thread-unfairness.sh

--- cpc_aware/thread-unfairness.sh
+++ cpc_aware/thread-unfairness.sh
@@ -26,7 +26,7 @@ done
 
 
 
-ssh ubuntu@$prob_vm "sysbench --threads=64 --time=220 cpu run"  >> "$OUTPUT_FILE"  &
+ssh ubuntu@$prob_vm "sysbench --threads=64 --time=240 cpu run"  >> "$OUTPUT_FILE"  &
 sleep 2
 sysbench_pid=$(ssh ubuntu@$prob_vm "pidof sysbench")
 

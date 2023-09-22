6412bc11c66649b112c73701a805fa85674ae4bf
FarWrong <55039915+FarWrong@users.noreply.github.com>
2023-09-22 14:40:45 -0400

    asdf

--- SMT_inaware/top_inaware-2.sh
+++ SMT_inaware/top_inaware-2.sh
@@ -5,7 +5,7 @@ sudo bash ../utility/cleanon_startup.sh $prob_vm 32
 io_benchmarks=("${io_benchmarks[@]}" "nginx-smt")
 io_benchmarks=("${io_benchmarks[@]}" "fio-smt")
 cpu_benchmarks=("${cpu_benchmarks[@]}" "sysbench-smt")
-cpu_benchmarks=("${cpu_benchmarks[@]}" "matmul-smt")
+cpu_benchmarks=("${cpu_benchmarks[@]}" "stressng-smt")
 
 OUTPUT_FILE="./tests/top_inaware_2_cpu$(date +%m%d%H%M).txt"
 OUTPUT_FILE2="./tests/top_inaware_2_io$(date +%m%d%H%M).txt"
@@ -24,15 +24,15 @@ test_smt_pair(){
     ssh ubuntu@$prob_vm "sudo killall sysbench" 
     ssh ubuntu@$prob_vm "sudo sysbench --threads=32 --time=10 cpu run" 
     #topology naive testing
-    
+    ssh ubuntu@$prob_vm "sudo echo '' > /tmp/waitingprocesses.tmp" 
 
     ssh ubuntu@$prob_vm "sudo phoronix-test-suite default-benchmark $cpu_bench" >> "$OUTPUT_FILE" & 
     ssh ubuntu@$prob_vm "sudo phoronix-test-suite default-benchmark $io_bench" >> "$OUTPUT_FILE2" 
     sleep 2
-    ssh ubuntu@$prob_vm "sudo sysbench --threads=32 --time=10 cpu run" 
     echo "running $cpu_bench smart">> $OUTPUT_FILE 
     echo "running $naive_bench smart">> $OUTPUT_FILE2
     #topology smart testing
+    ssh ubuntu@$prob_vm "sudo echo '' > /tmp/waitingprocesses.tmp" 
     ssh ubuntu@$prob_vm "sudo taskset -c 0-15 phoronix-test-suite default-benchmark $cpu_bench" >> "$OUTPUT_FILE" &
     ssh ubuntu@$prob_vm "sudo taskset -c 16-31 phoronix-test-suite default-benchmark $io_bench" >> "$OUTPUT_FILE2"  
 }

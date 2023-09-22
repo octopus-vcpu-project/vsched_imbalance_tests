76794733b3fa95f1665d16f683d5185e089aebc6
FarWrong <55039915+FarWrong@users.noreply.github.com>
2023-09-22 15:29:08 -0400

    Update pre.sh

--- SMT_inaware/test-profiles/nginx-smt/pre.sh
+++ SMT_inaware/test-profiles/nginx-smt/pre.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 affinity=$(taskset -pc $$ | awk -F:' ' '{print $2}')
-taskset -c affinity ./nginx_/sbin/nginx -g "worker_processes auto;"
+taskset -c $affinity ./nginx_/sbin/nginx -g "worker_processes auto;"
 sleep 5
-sudo echo "1" >>  /home/ubuntu/tmp/waitingprocesses.tmp && while [ "$(grep -c "1" /home/ubuntu/tmp/waitingprocesses.tmp 2>/dev/null)" -ge "2" ]; do sleep 0.5; done;

ba03de0fad50fc4b986beaf6cc8f25e37190eb89
FarWrong <55039915+FarWrong@users.noreply.github.com>
2023-09-22 15:16:17 -0400

    asdf

--- SMT_inaware/test-profiles/stressng-smt/pre.sh
+++ SMT_inaware/test-profiles/stressng-smt/pre.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo "1" >> /tmp/waitingprocesses.tmp && while [ "$(grep -c "1" /tmp/waitingprocesses.tmp 2>/dev/null)" -ge "2" ]; do sleep 0.5; done;
+sudo echo "1" >> /tmp/waitingprocesses.tmp && while [ "$(grep -c "1" /tmp/waitingprocesses.tmp 2>/dev/null)" -ge "2" ]; do sleep 0.5; done;

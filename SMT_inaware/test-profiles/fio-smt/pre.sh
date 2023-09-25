22fc691321bb62763ffab1377608c65174b9f036
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-09-25 13:23:08 -0400

    asdf

--- SMT_inaware/test-profiles/fio-smt/pre.sh
+++ SMT_inaware/test-profiles/fio-smt/pre.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 cd fio-3.35/
 rm -f fiofile
-sudo echo "1" >> /home/ubuntu/tmp/waitingprocesses.tmp && while [ "$(grep -c "1" /tmp/waitingprocesses.tmp 2>/dev/null)" -ge "2" ]; do sleep 0.5; done;
--- SMT_inaware/top_inaware-2.sh
+++ SMT_inaware/top_inaware-2.sh
@@ -2,15 +2,16 @@ declare -a io_benchmarks
 declare -a cpu_benchmarks
 prob_vm=$1
 sudo bash ../utility/cleanon_startup.sh $prob_vm 32
-io_benchmarks=("${io_benchmarks[@]}" "nginx-smt")
-io_benchmarks=("${io_benchmarks[@]}" "fio-smt")
-cpu_benchmarks=("${cpu_benchmarks[@]}" "sysbench-smt")
-cpu_benchmarks=("${cpu_benchmarks[@]}" "stressng-smt")
+io_benchmarks=("${io_benchmarks[@]}" "/var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1/wrk-4.2.0/wrk -d 90s -c 100 -t 16 https://127.0.0.1:8089/test.html")
+io_benchmarks=("${io_benchmarks[@]}" "fio --filename=/test --size=1GB --ioengine=libaio --iodepth=256 --runtime=90 --numjobs=16 --time_based --group_reporting --name=iops-test-job --eta-newline=1")
+cpu_benchmarks=("${cpu_benchmarks[@]}" "sysbench --threads=16 --time=90 cpu run ")
+cpu_benchmarks=("${cpu_benchmarks[@]}" "matmul.c")
 
 OUTPUT_FILE="./tests/top_inaware_2_cpu$(date +%m%d%H%M).txt"
 OUTPUT_FILE2="./tests/top_inaware_2_io$(date +%m%d%H%M).txt"
 ssh ubuntu@$prob_vm "mkdir /home/ubuntu/tmp/"
 ssh ubuntu@$prob_vm "touch /home/ubuntu/tmp/waitingprocesses.tmp"
+
 setup_phoronix_benchmark(){
     local bench=$1
     scp -r ./test-profiles/$bench ubuntu@$prob_vm:/tmp/
@@ -28,8 +29,8 @@ test_smt_pair(){
     #topology naive testing
     ssh ubuntu@$prob_vm "sudo echo '' > /home/ubuntu/tmp/waitingprocesses.tmp" 
 
-    ssh ubuntu@$prob_vm "sudo phoronix-test-suite default-benchmark $cpu_bench" >> "$OUTPUT_FILE" & 
-    ssh ubuntu@$prob_vm "sudo phoronix-test-suite default-benchmark $io_bench" >> "$OUTPUT_FILE2" 
+    ssh ubuntu@$prob_vm "sudo $cpu_bench" >> "$OUTPUT_FILE" & 
+    ssh ubuntu@$prob_vm "sudo $io_bench" >> "$OUTPUT_FILE2" 
     wait
     sleep 2
     echo "running $cpu_bench smart">> $OUTPUT_FILE 

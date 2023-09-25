f705db87cd05ee2910dd00874aae49abcff42820
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-09-25 15:00:51 -0400

    Update top_inaware-2.sh

--- SMT_inaware/top_inaware-2.sh
+++ SMT_inaware/top_inaware-2.sh
@@ -3,10 +3,10 @@ declare -a cpu_benchmarks
 prob_vm=$1
 sudo bash ../utility/cleanon_startup.sh $prob_vm 32
 
-io_benchmarks=("/var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1/wrk-4.2.0/wrk -d 90s -c 100 -t 16 https://127.0.0.1:8089/test.html")
-io_benchmarks+=("fio --filename=/test --size=1GB --ioengine=libaio --iodepth=256 --runtime=90 --numjobs=16 --time_based --group_reporting --name=iops-test-job --eta-newline=1")
-cpu_benchmarks=("sysbench --threads=16 --time=90 cpu run")
-cpu_benchmarks+=("~/vsched_tests/matmul.out 16 90")
+io_benchmarks=("sudo /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1/wrk-4.2.0/wrk -d 90s -c 100 -t 16 https://127.0.0.1:8089/test.html")
+io_benchmarks+=("sudo fio --filename=/test --size=1GB --ioengine=libaio --iodepth=256 --runtime=90 --numjobs=16 --time_based --group_reporting --name=iops-test-job --eta-newline=1")
+cpu_benchmarks=("sudo sysbench --threads=16 --time=90 cpu run")
+cpu_benchmarks+=("./vsched_tests/matmul.out 16 90")
 
 OUTPUT_FILE="./tests/top_inaware_2_cpu$(date +%m%d%H%M).txt"
 OUTPUT_FILE2="./tests/top_inaware_2_io$(date +%m%d%H%M).txt"
@@ -28,8 +28,8 @@ test_smt_pair() {
     
     ssh ubuntu@$prob_vm "sudo killall sysbench" 
     ssh ubuntu@$prob_vm "sudo sysbench --threads=32 --time=10 cpu run" 
-    ssh ubuntu@$prob_vm "sudo $cpu_bench" >> "$OUTPUT_FILE" & 
-    ssh ubuntu@$prob_vm "sudo $io_bench" >> "$OUTPUT_FILE2"
+    ssh ubuntu@$prob_vm "$cpu_bench" >> "$OUTPUT_FILE" & 
+    ssh ubuntu@$prob_vm "$io_bench" >> "$OUTPUT_FILE2"
     wait
     if [[ $io_bench == *nginx* ]]; then
         ssh ubuntu@$prob_vm "sudo killall nginx"
@@ -42,8 +42,8 @@ test_smt_pair() {
     echo "running $cpu_bench smart" >> $OUTPUT_FILE 
     echo "running $io_bench smart" >> $OUTPUT_FILE2 # changed $naive_bench to $io_bench
 
-    ssh ubuntu@$prob_vm "sudo taskset -c 0-15 $cpu_bench" >> "$OUTPUT_FILE" & 
-    ssh ubuntu@$prob_vm "sudo taskset -c 16-31 $io_bench" >> "$OUTPUT_FILE2"
+    ssh ubuntu@$prob_vm "taskset -c 0-15 $cpu_bench" >> "$OUTPUT_FILE" & 
+    ssh ubuntu@$prob_vm "taskset -c 16-31 $io_bench" >> "$OUTPUT_FILE2"
     wait
     ssh ubuntu@$prob_vm "sudo killall nginx"
     ssh ubuntu@$prob_vm "sudo killall php"

419d29812b157fc1612838f41e42aaa3adbf5a2d
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-09-26 18:14:21 -0400

    asdf

--- NUMA_inaware-1/top_inaware-2.sh
+++ NUMA_inaware-1/top_inaware-2.sh
@@ -2,20 +2,49 @@ prob_vm = $1
 comm_benchmark = "sysbench --threads=16 --time=100 cpu run"
 ../utility/cleanon_startup.sh prob_vm 32
 
+
+toggle_topological_passthrough(){
+    virsh shutdown $prob_vm
+    while true; do
+        vm_state=$(virsh domstate "$prob_vm")
+        if [ "$vm_state" != "running" ]; then
+            echo "VM is shutdown"
+            break
+        else
+            echo "Waiting for VM to shutdown"
+            sleep 3 
+        fi
+    done
+    virsh dumpxml $prob_vm > /tmp/$prob_vm.xml
+    if [ $1 -eq 1 ]; then
+        sed -i "/<cpu /,/<\/cpu>/c\\$smart_topology_string" /tmp/$prob_vm.xml
+    else
+        sed -i "/<cpu /,/<\/cpu>/c\\$naive_topology_string" /tmp/$prob_vm.xml
+    fi
+    virsh define /tmp/$prob_vm.xml
+    sudo bash ../utility/cleanon_startup.sh $prob_vm 32
+}
+
+
 for i in {0..15};do
     sudo virsh vcpupin $i $i
 done
 
 for i in {16..31};do
-    sudo virsh vcpupin $i $((i + 20))
+    sudo virsh vcpupin $i $((i + 4))
 done
 
 ssh ubuntu@$prob_vm "sudo killall sysbench" 
-
-#topology naive testing
-OUTPUT_FILE="top_inaware_2_cpu_naive$(date +%m%d%H%M).txt"
-ssh ubuntu@$prob_vm "sudo $comm_benchmark" > "$OUTPUT_FILE" &
+toggle_topological_passthrough 0
+#blind
+OUTPUT_FILE="./tests/numa_unpinned$(date +%m%d%H%M).txt"
+ssh ubuntu@$prob_vm "sudo $comm_benchmark" > "$OUTPUT_FILE" 
 sleep 3
-#topology smart testing
-OUTPUT_FILE="top_inaware_2_cpu_smart$(date +%m%d%H%M).txt"
-ssh ubuntu@$prob_vm "sudo taskset -c 0-15 $cpu_benchmark" > "$OUTPUT_FILE" &
+OUTPUT_FILE2="./tests/numa_pinned$(date +%m%d%H%M).txt"
+ssh ubuntu@$prob_vm "sudo taskset -c 0-16 $comm_benchmark" > "$OUTPUT_FILE2" 
+sleep 3
+toggle_topological_passthrough 1
+#passthrough
+ssh ubuntu@$prob_vm "sudo $comm_benchmark" > "$OUTPUT_FILE" 
+sleep 3
+ssh ubuntu@$prob_vm "sudo taskset -c 0-16 $comm_benchmark" > "$OUTPUT_FILE2" 
--- SMT_inaware/parse-perf.py
+++ SMT_inaware/parse-perf.py
@@ -1,54 +1,97 @@
-import glob
+import re
 import matplotlib.pyplot as plt
-# Find all files that match the 'sym-plc*' pattern
-files = glob.glob("./tests/top_plc_smart*.txt")
+import numpy as np
+cpu_file = './tests/top_inaware_2_cpu09251738.txt'  # Replace with your actual file name
 
-# Sort the files to get the latest one
-files.sort(reverse=True)
+io_file = './tests/top_inaware_2_io09251738.txt'  # Replace with your actual file name
 
-# Read the latest file if one exists
-if files:
-    with open(files[0], 'r') as f:
-        print(f"Reading {files[0]}")
-        print(f.read())
-else:
-    print("No matching files found.")
+amount_of_tests = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+cpu_increment = 0
+io_increment = 0
+with open(cpu_file, 'r') as file:
+    cpulines = file.readlines()
 
-# Initialize an empty list to hold the counts
-cpu_sysbench_counts = []
+species = ["", "", "","","","","",""]
+penguin_means = {
+    'CPU': [],
+    'IO': [],
+}
 
-# Read the latest file if one exists
-if files:
-    with open(files[0], 'r') as f:
-        lines = f.readlines()
-        current_cpu = -1
-        used_phys_cpus = []
-        incorrect = 0
-        incorrect_list = []
-        for line in lines:
-            # Update the current CPU if we see a "cpu#X" line
-            if line.startswith("cpu#"):
-                current_cpu = int(line.split('#')[1].split(',')[0])
-           
-                while len(cpu_sysbench_counts) <= current_cpu:
-                    cpu_sysbench_counts.append(0)
-                print(current_cpu)
-                if(current_cpu==31):
-                        incorrect_list.append(incorrect)
-                        used_phys_cpus = []
-                        incorrect=0
-            # Count 'sysbench' occurrences for the current CPU
-            elif "sysbench" in line:
-                if current_cpu != -1:
-                    if(not (current_cpu//2) in used_phys_cpus):
-                        incorrect += 1
-                        used_phys_cpus.append(current_cpu//2)
-                    cpu_sysbench_counts[current_cpu] += 1
-    print("Physical cores used", incorrect_list)
-    plt.bar(range(len(cpu_sysbench_counts)), cpu_sysbench_counts)
-    plt.xlabel('CPU Number')
-    plt.ylabel('Number of sysbench Occurrences')
-    plt.title('sysbench Occurrences by CPU')
-    plt.show()
-else:
-    print("No matching files found.")
+i = 0
+while i < len(cpulines):
+    line = cpulines[i].strip()
+    if "total number of events" in line:
+        match = re.search(r"total number of events:\s+(\d+)", line)
+        if match:
+            print("Sysbench: ", match.group(1))
+            amount_of_tests[cpu_increment // 2][(cpu_increment % 2)* 2 + 1] = match.group(1)
+            species[cpu_increment] = species[cpu_increment] + "+Sysbench"
+            penguin_means["CPU"].append(int(match.group(1)))
+            cpu_increment+=1
+            
+    elif "computing slice" in line:
+        matmul_sum = 0
+        while "computing slice" in line or "finished slice" in line or re.match(r"\d+\s+\d+", line):
+            if re.match(r"\d+\s+\d+", line):
+                _, value = map(int, line.split())
+                matmul_sum += value
+            i += 1
+            if i < len(cpulines):
+                line = cpulines[i].strip()
+            else:
+                break
+        print("Matmul Sum: ", matmul_sum)
+        if(matmul_sum!= 0):
+            amount_of_tests[cpu_increment // 2][(cpu_increment % 2)* 2 + 1] = matmul_sum
+            species[cpu_increment] = species[cpu_increment] + "+MATMUL"
+            penguin_means["CPU"].append(int(matmul_sum))
+            cpu_increment+=1
+        continue  # Skip the increment as we already do it in the inner loop
+    i += 1
+
+
+with open(io_file, 'r') as file:
+    iolines = file.readlines()
+i = 0
+while i < len(iolines):
+    line = iolines[i].strip()
+    if "Transfer/sec" in line:
+        match = re.search(r"Transfer/sec:\s+(\d+)", line)
+        if match:
+            print("Transfers: ", match.group(1))
+            amount_of_tests[io_increment // 2][(io_increment % 2)* 2 ] = match.group(1)
+            species[io_increment] = species[io_increment] + "+NGINX"
+            penguin_means["IO"].append(int(match.group(1)))
+            io_increment+=1
+        
+    
+    elif "READ: bw=" in line:
+        match = re.search(r"bw=(\d+)", line)
+        if match:
+            print("rdw: ", match.group(1))
+            amount_of_tests[io_increment // 2][(io_increment % 2)* 2 ] = match.group(1)
+            species[io_increment] = species[io_increment] + "+FIO"
+            penguin_means["IO"].append(int(match.group(1)))
+            io_increment+=1
+        # Skip the increment as we already do it in the inner loop
+    i += 1
+print(penguin_means)
+x = np.arange(len(species))  # the label locations
+width = 0.25  # the width of the bars
+multiplier = 0
+
+fig, ax = plt.subplots(layout='constrained')
+
+for attribute, measurement in penguin_means.items():
+    offset = width * multiplier
+    rects = ax.bar(x + offset, measurement, width, label=attribute)
+    ax.bar_label(rects, padding=3)
+    multiplier += 1
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Length (mm)')
+ax.set_title('Penguin attributes by species')
+ax.set_xticks(x + width, species)
+ax.legend(loc='upper left', ncols=3)
+
+plt.show()

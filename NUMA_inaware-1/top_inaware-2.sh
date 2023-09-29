dfbfe857460ab085906a2e707ebd1dad6457785e
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-09-29 17:25:56 -0400

    asdf

--- NUMA_inaware-1/top_inaware-2.sh
+++ NUMA_inaware-1/top_inaware-2.sh
@@ -45,12 +45,12 @@ toggle_topological_passthrough 0
 OUTPUT_FILE="./tests/numa_blind$(date +%m%d%H%M).txt"
 
 ssh ubuntu@$prob_vm "sudo /home/ubuntu/bpftrace/build/src/bpftrace -e 'kfunc:native_send_call_func_single_ipi { @[cpu] = count(); }' &" >> "$OUTPUT_FILE" &
-ssh ubuntu@$prob_vm "sudo $cpu_benchmark &" &
+#ssh ubuntu@$prob_vm "sudo $cpu_benchmark &" &
 ssh ubuntu@$prob_vm "sudo $comm_benchmark" >> "$OUTPUT_FILE" 
 ssh ubuntu@$prob_vm "sudo killall bpftrace;sudo killall sysbench" 
 echo "test finished"
 ssh ubuntu@$prob_vm "sudo /home/ubuntu/bpftrace/build/src/bpftrace -e 'kfunc:native_send_call_func_single_ipi { @[cpu] = count(); }' &" >> "$OUTPUT_FILE" &
-ssh ubuntu@$prob_vm "sudo taskset -c 0-15 $cpu_benchmark &" &
+#ssh ubuntu@$prob_vm "sudo taskset -c 0-15 $cpu_benchmark &" &
 ssh ubuntu@$prob_vm "sudo taskset -c 16-31 $comm_benchmark" >> "$OUTPUT_FILE" 
 ssh ubuntu@$prob_vm "sudo killall bpftrace;sudo killall sysbench"
 echo "test finished"
@@ -59,12 +59,12 @@ toggle_topological_passthrough 1
 #passthrough
 OUTPUT_FILE="./tests/numa_smart$(date +%m%d%H%M).txt"
 ssh ubuntu@$prob_vm "sudo /home/ubuntu/bpftrace/build/src/bpftrace -e 'kfunc:native_send_call_func_single_ipi { @[cpu] = count(); }' &" >> "$OUTPUT_FILE" &
-ssh ubuntu@$prob_vm "sudo $cpu_benchmark &" &
+#ssh ubuntu@$prob_vm "sudo $cpu_benchmark &" &
 ssh ubuntu@$prob_vm "sudo $comm_benchmark" >> "$OUTPUT_FILE" 
 ssh ubuntu@$prob_vm "sudo killall bpftrace;sudo killall sysbench" 
 echo "test finished"
 ssh ubuntu@$prob_vm "sudo /home/ubuntu/bpftrace/build/src/bpftrace -e 'kfunc:native_send_call_func_single_ipi { @[cpu] = count(); }' &" >> "$OUTPUT_FILE" &
-ssh ubuntu@$prob_vm "sudo taskset -c 0-15 $cpu_benchmark &" &
+#ssh ubuntu@$prob_vm "sudo taskset -c 0-15 $cpu_benchmark &" &
 ssh ubuntu@$prob_vm "sudo taskset -c 16-31 $comm_benchmark" >> "$OUTPUT_FILE" 
 ssh ubuntu@$prob_vm "sudo killall bpftrace;sudo killall sysbench" 
 sleep 3

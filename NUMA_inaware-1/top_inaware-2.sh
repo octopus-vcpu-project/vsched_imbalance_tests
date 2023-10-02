f270354d532ce963d4635d9a99f2796c087421fb
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-10-02 17:47:45 -0400

    Update top_inaware-2.sh

--- NUMA_inaware-1/top_inaware-2.sh
+++ NUMA_inaware-1/top_inaware-2.sh
@@ -3,7 +3,7 @@ comm_benchmark="sudo /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.
 sudo bash ../utility/cleanon_startup.sh $prob_vm 32
 naive_topology_string="<cpu mode='custom' match='exact' check='none'>\n<model fallback='forbid'>qemu64</model>\n</cpu>"
 smart_topology_string="<cpu mode='custom' match='exact' check='none'>\n    <model fallback='forbid'>qemu64</model>\n    <topology sockets='2' dies='1' cores='16' threads='1'/></cpu>"
-
+#kvm_hv_send_ipi
 #what
 toggle_topological_passthrough(){
     virsh shutdown $prob_vm
@@ -49,13 +49,15 @@ run_numa_test(){
         ssh ubuntu@$prob_vm "cd /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1;sudo ./nginx_/sbin/nginx -g 'worker_processes auto;'"
         sleep 5
     fi
+    
+    ssh ubuntu@$prob_vm "sudo $comm_bench" >> "$OUTPUT_FILE" &
+    ssh ubuntu@$prob_vm "sudo $comm_bench" >> "$OUTPUT_FILE2" &
+    ssh ubuntu@$prob_vm "sudo killall bpftrace;"
     if [ $trace_bpf != 0 ]; then
         #ssh ubuntu@$prob_vm "sudo /home/ubuntu/bpftrace/build/src/bpftrace -e 'kfunc:native_send_call_func_single_ipi { @[cpu] = count(); }' &" >> "$OUTPUT_FILE" &
-        sudo perf stat -B -C 0-15,20-35 -o "$OUTPUT_FILE3"  -e cache:* sleep 30 &
+        sudo perf stat -B -C 0-15,20-35 -o "$OUTPUT_FILE3"  -e L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,LLC-loads,LLC-load-misses,LLC-stores,LLC-prefetches,cache-references,cache-misses,cycles,instructions,branches,faults,migrations sleep 30 &
     fi
-    ssh ubuntu@$prob_vm "sudo $comm_bench" >> "$OUTPUT_FILE" &
-    ssh ubuntu@$prob_vm "sudo $comm_bench" >> "$OUTPUT_FILE2" 
-    ssh ubuntu@$prob_vm "sudo killall bpftrace;"
+    wait
     sudo killall perf
     sleep 4
     toggle_topological_passthrough 1
@@ -64,13 +66,15 @@ run_numa_test(){
         ssh ubuntu@$prob_vm "cd /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1;sudo ./nginx_/sbin/nginx -g 'worker_processes auto;'"
         sleep 5
     fi
-    if [ $trace_bpf != 0 ]; then
-        #ssh ubuntu@$prob_vm "sudo /home/ubuntu/bpftrace/build/src/bpftrace -e 'kfunc:native_send_call_func_single_ipi { @[cpu] = count(); }' &" >> "$OUTPUT_FILE" &
-        sudo perf stat -B -C 0-15,20-35 -o "$OUTPUT_FILE4"  -e   cache:* sleep 30 &
-    fi
+    
     ssh ubuntu@$prob_vm "sudo $comm_bench" >> "$OUTPUT_FILE" &
     ssh ubuntu@$prob_vm "sudo $comm_bench" >> "$OUTPUT_FILE2" 
+    if [ $trace_bpf != 0 ]; then
+        #ssh ubuntu@$prob_vm "sudo /home/ubuntu/bpftrace/build/src/bpftrace -e 'kfunc:native_send_call_func_single_ipi { @[cpu] = count(); }' &" >> "$OUTPUT_FILE" &
+        sudo perf stat -B -C 0-15,20-35 -o "$OUTPUT_FILE4"  -e L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,LLC-loads,LLC-load-misses,LLC-stores,LLC-prefetches,cache-references,cache-misses,cycles,instructions,branches,faults,migrations sleep 30 &
+    fi
     ssh ubuntu@$prob_vm "sudo killall bpftrace;"
+    wait
     sleep 4
 }
 

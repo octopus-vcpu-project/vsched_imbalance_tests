a56a7f1d66a579e60c3374bb4c967676bbdd7df1
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-10-02 18:07:08 -0400

    Update top_inaware-2.sh

--- NUMA_inaware-1/top_inaware-2.sh
+++ NUMA_inaware-1/top_inaware-2.sh
@@ -55,7 +55,7 @@ run_numa_test(){
     ssh ubuntu@$prob_vm "sudo killall bpftrace;"
     if [ $trace_bpf != 0 ]; then
         #ssh ubuntu@$prob_vm "sudo /home/ubuntu/bpftrace/build/src/bpftrace -e 'kfunc:native_send_call_func_single_ipi { @[cpu] = count(); }' &" >> "$OUTPUT_FILE" &
-        sudo perf record -B -C 0-15,20-35 -o "$OUTPUT_FILE4"  -e l2_rqsts.miss,l2_rqsts.references,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,LLC-loads,LLC-load-misses,LLC-stores,LLC-prefetches,cache-references,cache-misses,cycles,instructions,branches,faults,migrations ssh ubuntu@$prob_vm "sudo $comm_bench & sudo $comm_bench" >> "$OUTPUT_FILE2" 
+        sudo perf record -B -C 0-15,20-35 -o "$OUTPUT_FILE4"  -e l2_rqsts.miss,l2_rqsts.references,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,LLC-loads,LLC-load-misses,LLC-stores,cache-references,cache-misses,cycles,instructions,branches,faults,migrations ssh ubuntu@$prob_vm "sudo $comm_bench & sudo $comm_bench" >> "$OUTPUT_FILE2" 
     fi
     wait
     sudo killall perf
@@ -71,7 +71,7 @@ run_numa_test(){
     
     if [ $trace_bpf != 0 ]; then
         #ssh ubuntu@$prob_vm "sudo /home/ubuntu/bpftrace/build/src/bpftrace -e 'kfunc:native_send_call_func_single_ipi { @[cpu] = count(); }' &" >> "$OUTPUT_FILE" &
-        sudo perf record -B -C 0-15,20-35 -o "$OUTPUT_FILE4"  -e l2_rqsts.miss,l2_rqsts.references,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,LLC-loads,LLC-load-misses,LLC-stores,LLC-prefetches,cache-references,cache-misses,cycles,instructions,branches,faults,migrations ssh ubuntu@$prob_vm "sudo $comm_bench & sudo $comm_bench" >> "$OUTPUT_FILE2" 
+        sudo perf record -B -C 0-15,20-35 -o "$OUTPUT_FILE4"  -e l2_rqsts.miss,l2_rqsts.references,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,LLC-loads,LLC-load-misses,LLC-stores,cache-references,cache-misses,cycles,instructions,branches,faults,migrations ssh ubuntu@$prob_vm "sudo $comm_bench & sudo $comm_bench" >> "$OUTPUT_FILE2" 
     fi
     ssh ubuntu@$prob_vm "sudo killall bpftrace;"
     wait

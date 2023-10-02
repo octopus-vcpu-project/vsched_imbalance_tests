5bc0ad91fa9558129008c0ca5d837e4e2b411f32
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-10-02 19:22:11 -0400

    Update top_inaware-2.sh

--- NUMA_inaware-1/top_inaware-2.sh
+++ NUMA_inaware-1/top_inaware-2.sh
@@ -1,5 +1,5 @@
 prob_vm=$1
-comm_benchmark="/home/ubuntu/Workloads/parsec-benchmark/bin/parsecmgmt -a run -p dedup -n 32 -i native" 
+comm_benchmark="sudo /home/ubuntu/Workloads/parsec-benchmark/bin/parsecmgmt -a run -p dedup -n 32 -i native" 
 cpu_benchmark="sysbench --threads=16 --time=10000 cpu run"
 sudo bash ../utility/cleanon_startup.sh $prob_vm 32
 naive_topology_string="<cpu mode='custom' match='exact' check='none'>\n<model fallback='forbid'>qemu64</model>\n</cpu>"

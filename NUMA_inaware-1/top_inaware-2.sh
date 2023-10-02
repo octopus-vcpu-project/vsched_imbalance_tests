cf76c467630df24dcbfe4764443d80cd791e0902
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-10-02 17:33:48 -0400

    Update top_inaware-2.sh

--- NUMA_inaware-1/top_inaware-2.sh
+++ NUMA_inaware-1/top_inaware-2.sh
@@ -51,7 +51,7 @@ run_numa_test(){
     fi
     if [ $trace_bpf != 0 ]; then
         #ssh ubuntu@$prob_vm "sudo /home/ubuntu/bpftrace/build/src/bpftrace -e 'kfunc:native_send_call_func_single_ipi { @[cpu] = count(); }' &" >> "$OUTPUT_FILE" &
-        sudo perf stat -B -C 0-15,20-35 -o "$OUTPUT_FILE3"  -e  L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,LLC-loads,LLC-load-misses,LLC-stores,LLC-prefetches,cache-references,cache-misses,cycles,instructions,branches,faults,migrations sleep 30 
+        sudo perf stat -B -C 0-15,20-35 -o "$OUTPUT_FILE3"  -e  L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,LLC-loads,LLC-load-misses,LLC-stores,LLC-prefetches,cache-references,cache-misses,cycles,instructions,branches,faults,migrations sleep 30 &
     fi
     ssh ubuntu@$prob_vm "sudo $comm_bench" >> "$OUTPUT_FILE" &
     ssh ubuntu@$prob_vm "sudo $comm_bench" >> "$OUTPUT_FILE2" 
@@ -66,7 +66,7 @@ run_numa_test(){
     fi
     if [ $trace_bpf != 0 ]; then
         #ssh ubuntu@$prob_vm "sudo /home/ubuntu/bpftrace/build/src/bpftrace -e 'kfunc:native_send_call_func_single_ipi { @[cpu] = count(); }' &" >> "$OUTPUT_FILE" &
-        sudo perf stat -B -C 0-15,20-35 -o "$OUTPUT_FILE4"  -e   LLC-loads,LLC-load-misses,LLC-stores,LLC-prefetches,cache-references,cache-misses,cycles,instructions,branches,faults,migrations sleep 30 
+        sudo perf stat -B -C 0-15,20-35 -o "$OUTPUT_FILE4"  -e   LLC-loads,LLC-load-misses,LLC-stores,LLC-prefetches,cache-references,cache-misses,cycles,instructions,branches,faults,migrations sleep 30 &
     fi
     ssh ubuntu@$prob_vm "sudo $comm_bench" >> "$OUTPUT_FILE" &
     ssh ubuntu@$prob_vm "sudo $comm_bench" >> "$OUTPUT_FILE2" 

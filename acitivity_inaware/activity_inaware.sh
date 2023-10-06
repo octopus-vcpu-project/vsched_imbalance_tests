8e0e4a027a639f8c9582e232c16c3655a3ef9b2a
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-10-06 13:33:10 -0400

    Update activity_inaware.sh

--- acitivity_inaware/activity_inaware.sh
+++ acitivity_inaware/activity_inaware.sh
@@ -21,25 +21,26 @@ wake_and_pin_vm $prob_vm
 ssh ubuntu@$prob_vm "sudo python /home/ubuntu/bpftrace/bcc/tools/runqlat.py ">> "$OUTPUT_FILE" &
 ssh ubuntu@$prob_vm "sudo $main_command" &
 sleep 60
-ssh ubuntu@$prob_vm "sudo kill -s SIGINT $(pidof python)"
+
+ssh ubuntu@$prob_vm "sudo kill -s SIGINT \$(pidof python)"
 sleep 3
 wake_and_pin_vm $compete_vm1
 ssh ubuntu@$compete_vm1 "sudo $main_command" &
 ssh ubuntu@$prob_vm "sudo python /home/ubuntu/bpftrace/bcc/tools/runqlat.py ">> "$OUTPUT_FILE" &
 sleep 60
-ssh ubuntu@$prob_vm "sudo kill -s SIGINT $(pidof python)"
+ssh ubuntu@$prob_vm "sudo kill -s SIGINT \$(pidof python)"
 sleep 3
 wake_and_pin_vm $compete_vm2
 ssh ubuntu@$compete_vm2 "sudo $main_command" &
 ssh ubuntu@$prob_vm "sudo python /home/ubuntu/bpftrace/bcc/tools/runqlat.py ">> "$OUTPUT_FILE" &
 sleep 60
-ssh ubuntu@$prob_vm "sudo kill -s SIGINT $(pidof python)"
+ssh ubuntu@$prob_vm "sudo kill -s SIGINT \$(pidof python)"
 sleep 3
 wake_and_pin_vm $compete_vm3
 ssh ubuntu@$compete_vm3 "sudo $main_command" &
 ssh ubuntu@$prob_vm "sudo python /home/ubuntu/bpftrace/bcc/tools/runqlat.py ">> "$OUTPUT_FILE" &
 sleep 60
-ssh ubuntu@$prob_vm "sudo kill -s SIGINT $(pidof python)"
+ssh ubuntu@$prob_vm "sudo kill -s SIGINT \$(pidof python)"
 sleep 3
 
 

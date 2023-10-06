a0350ce65fe35e61c76bfb6f3ba3e88e3f413494
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-10-06 17:12:27 -0400

    Update top_inaware-2.sh

--- NUMA_inaware-1/top_inaware-2.sh
+++ NUMA_inaware-1/top_inaware-2.sh
@@ -71,19 +71,22 @@ PERF_OUTPUT2="./tests/perf_out2$(date +%m%d%H%M).txt"
 #ssh ubuntu@$prob_vm "sudo kill -s SIGINT \$(pidof python)"
 #sleep 3
 #ssh ubuntu@$prob_vm "sudo killall sysbench"
-ssh ubuntu@$prob_vm "sudo killall nginx"
-ssh ubuntu@$prob_vm "cd /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1;taskset -c 0-3,16-19 sudo ./nginx_/sbin/nginx -g 'worker_processes auto;'"
-sleep 5
-sudo perf stat -B -C 0-15,20-35 -o "$PERF_OUTPUT"  -e l2_rqsts.miss,l2_rqsts.references,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,LLC-loads,LLC-load-misses,LLC-stores,cache-references,cache-misses,cycles,instructions,branches,faults,migrations ssh ubuntu@$prob_vm "taskset -c 0-3,16-19 sudo $comm_benchmark" 
-ssh ubuntu@$prob_vm "sudo killall nginx"
-ssh ubuntu@$prob_vm "cd /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1;taskset -c 16-23 sudo ./nginx_/sbin/nginx -g 'worker_processes auto;'"
-sleep 5
-sudo perf stat -B -C 0-15,20-35 -o "$PERF_OUTPUT2"  -e l2_rqsts.miss,l2_rqsts.references,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,LLC-loads,LLC-load-misses,LLC-stores,cache-references,cache-misses,cycles,instructions,branches,faults,migrations ssh ubuntu@$prob_vm "taskset -c 16-23 sudo $comm_benchmark" 
+
+ssh ubuntu@$prob_vm "sudo $comm_benchmark" &
+ssh ubuntu@$prob_vm "sudo $comm_benchmark"
+for i in {0..40};do 
+    sleep 1
+    ssh ubuntu@$prob_vm "sudo cat /sys/kernel/debug/sched/debug | grep -E 'cpu#|>R '" >> "$OUTPUT_FILE"
+done
 
 toggle_topological_passthrough 1
 #ssh ubuntu@$prob_vm "sudo /home/ubuntu/bpftrace/build/src/bpftrace -e 'kfunc:native_send_call_func_single_ipi { @[cpu] = count(); }' &" >> "$OUTPUT_FILE" &
-#ssh ubuntu@$prob_vm "sudo $comm_benchmark" >> "$OUTPUT_FILE" &
-#ssh ubuntu@$prob_vm "sudo $comm_benchmark" >> "$OUTPUT_FILE2" 
+ssh ubuntu@$prob_vm "sudo $comm_benchmark" &
+ssh ubuntu@$prob_vm "sudo $comm_benchmark"
+for i in {0..40};do 
+    sleep 1
+    ssh ubuntu@$prob_vm "sudo cat /sys/kernel/debug/sched/debug | grep -E 'cpu#|>R '" >> "$OUTPUT_FILE"
+done
 #ssh ubuntu@$prob_vm "sudo killall bpftrace;sudo killall sysbench" 
 #echo "test finished"
 #sleep 8

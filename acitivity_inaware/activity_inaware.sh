3ec8ecdf440d063cb1a46672d9fa957e26f44a35
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-10-06 13:37:02 -0400

    Update activity_inaware.sh

--- acitivity_inaware/activity_inaware.sh
+++ acitivity_inaware/activity_inaware.sh
@@ -3,7 +3,7 @@ prob_vm=$1
 compete_vm1=$2
 compete_vm2=$3
 compete_vm3=$4
-
+benchmark_time=20
 main_command="sysbench --threads=32 --time=30000 cpu run"
 
 OUTPUT_FILE="./tests/acitivity_inaware-1$(date +%m%d%H%M).txt"
@@ -20,30 +20,32 @@ wake_and_pin_vm $prob_vm
 
 ssh ubuntu@$prob_vm "sudo python /home/ubuntu/bpftrace/bcc/tools/runqlat.py ">> "$OUTPUT_FILE" &
 ssh ubuntu@$prob_vm "sudo $main_command" &
-sleep 60
+sleep $benchmark_time
 
 ssh ubuntu@$prob_vm "sudo kill -s SIGINT \$(pidof python)"
-sleep 3
+sleep $benchmark_time
 wake_and_pin_vm $compete_vm1
 ssh ubuntu@$compete_vm1 "sudo $main_command" &
 ssh ubuntu@$prob_vm "sudo python /home/ubuntu/bpftrace/bcc/tools/runqlat.py ">> "$OUTPUT_FILE" &
-sleep 60
+sleep $benchmark_time
 ssh ubuntu@$prob_vm "sudo kill -s SIGINT \$(pidof python)"
 sleep 3
 wake_and_pin_vm $compete_vm2
 ssh ubuntu@$compete_vm2 "sudo $main_command" &
 ssh ubuntu@$prob_vm "sudo python /home/ubuntu/bpftrace/bcc/tools/runqlat.py ">> "$OUTPUT_FILE" &
-sleep 60
+sleep $benchmark_time
 ssh ubuntu@$prob_vm "sudo kill -s SIGINT \$(pidof python)"
 sleep 3
 wake_and_pin_vm $compete_vm3
 ssh ubuntu@$compete_vm3 "sudo $main_command" &
 ssh ubuntu@$prob_vm "sudo python /home/ubuntu/bpftrace/bcc/tools/runqlat.py ">> "$OUTPUT_FILE" &
-sleep 60
+sleep $benchmark_time
 ssh ubuntu@$prob_vm "sudo kill -s SIGINT \$(pidof python)"
 sleep 3
-
-
+ssh ubuntu@$prob_vm "sudo killall sysbench"
+virsh shutdown $compete_vm1
+virsh shutdown $compete_vm2
+virsh shutdown $compete_vm3
 
 sudo git add .;sudo git commit -m 'new';sudo git push
 

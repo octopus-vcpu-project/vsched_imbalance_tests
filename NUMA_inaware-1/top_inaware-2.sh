8585156da680a62254e88691a4c3abfd4faeb9e1
FarWrong <55039915+FarWrong@users.noreply.github.com>
2023-10-09 14:16:05 -0700

    Update top_inaware-2.sh

--- NUMA_inaware-1/top_inaware-2.sh
+++ NUMA_inaware-1/top_inaware-2.sh
@@ -63,7 +63,7 @@ sleep 10
 
 echo "raw performance test complete"
 
-sudo perf stat -B -o "$PERF_OUTPUT" -C 20-60 -e LLC-loads,LLC-load-misses,LLC-stores,cache-references,cache-misses,cycles,instructions sleep 4000000000
+sudo perf stat -B -o "$PERF_OUTPUT" -C 20-60 -e LLC-loads,LLC-load-misses,LLC-stores,cache-references,cache-misses,cycles,instructions sleep 4000000000 &
 ssh ubuntu@$prob_vm "sudo $comm_benchmark & sudo $comm_benchmark_1" 
 ssh ubuntu@$prob_vm "sudo kill -s SIGINT \$(pidof perf)"
 
@@ -90,7 +90,7 @@ ssh ubuntu@$prob_vm "sudo $comm_benchmark_1" >> "$OUTPUT_FILE2"
 echo "test finished"
 sleep 10
 
-sudo perf stat -B -o "$PERF_OUTPUT2" -C 20-60 -e LLC-loads,LLC-load-misses,LLC-stores,cache-references,cache-misses,cycles,instructions sleep 4000000000
+sudo perf stat -B -o "$PERF_OUTPUT2" -C 20-60 -e LLC-loads,LLC-load-misses,LLC-stores,cache-references,cache-misses,cycles,instructions sleep 4000000000 &
 ssh ubuntu@$prob_vm "sudo $comm_benchmark & sudo $comm_benchmark_1" 
 ssh ubuntu@$prob_vm "sudo kill -s SIGINT \$(pidof perf)"
 

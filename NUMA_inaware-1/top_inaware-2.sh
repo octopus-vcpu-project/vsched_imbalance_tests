8411a92fc712d56c804754ddf125705708703905
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-10-09 16:05:40 -0400

    Update top_inaware-2.sh

--- NUMA_inaware-1/top_inaware-2.sh
+++ NUMA_inaware-1/top_inaware-2.sh
@@ -27,7 +27,7 @@ toggle_topological_passthrough(){
         sed -i "/<cpu /,/<\/cpu>/c\\$naive_topology_string" /tmp/$prob_vm.xml
     fi
     virsh define /tmp/$prob_vm.xml
-    sudo bash ../utility/cleanon_startup.sh $prob_vm 24
+    sudo bash ../utility/cleanon_startup.sh $prob_vm 40
     for i in {0..19};do
         sudo virsh vcpupin $prob_vm $i $((i + 20))
     done

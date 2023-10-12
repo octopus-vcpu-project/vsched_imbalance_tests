827524f1a41e31777d639f10e489921adb565f9c
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-10-12 18:54:05 -0400

    Update top_inaware-2.sh

--- NUMA_inaware-1/top_inaware-2.sh
+++ NUMA_inaware-1/top_inaware-2.sh
@@ -3,11 +3,11 @@ prob_vm=$1
 comm_benchmark="sudo /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1/wrk-4.2.0/wrk -d 90s -c 300 -t 8 https://127.0.0.1:8089/test.html"
 cpu_benchmark="sysbench --threads=16 --time=10000 cpu run"
 virsh shutdown $prob_vm
-sudo bash ../utility/cleanon_startup.sh $prob_vm 32
+sudo bash ../utility/cleanon_startup.sh $prob_vm 20
 naive_topology_string="<cpu mode='custom' match='exact' check='none'>\n<model fallback='forbid'>qemu64</model>\n</cpu>"
 smart_topology_string="<cpu mode='custom' match='exact' check='none'>\n    <model fallback='forbid'>qemu64</model>\n    <topology sockets='2' dies='1' cores='16' threads='1'/></cpu>"
 #comm_benchmark_1="/var/lib/phoronix-test-suite/installed-tests/pts/new-nginx-3/wrk-4.2.0/wrk -d 60s -c 300 -t 8 https://127.0.0.1:8089/test.html" 
-comm_benchmark_1=$comm_benchmark
+comm_benchmark_1="sleep 120"
 
 toggle_topological_passthrough(){
     virsh shutdown $prob_vm
@@ -28,14 +28,14 @@ toggle_topological_passthrough(){
         sed -i "/<cpu /,/<\/cpu>/c\\$naive_topology_string" /tmp/$prob_vm.xml
     fi
     virsh define /tmp/$prob_vm.xml
-    sudo bash ../utility/cleanon_startup.sh $prob_vm 32
-    for i in {0..15};do
+    sudo bash ../utility/cleanon_startup.sh $prob_vm 20
+    for i in {0..20};do
         sudo virsh vcpupin $prob_vm $i $((i + 20))
     done
 
-    for i in {16..31};do
-        sudo virsh vcpupin $prob_vm $i $((i + 24))
-    done
+    #for i in {16..31};do
+    #    sudo virsh vcpupin $prob_vm $i $((i + 24))
+    #done
     echo "Pinning Complete"
    ssh ubuntu@$prob_vm "sudo killall nginx"
    ssh ubuntu@$prob_vm "cd /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1;sudo ./nginx_/sbin/nginx -g 'worker_processes auto;'"
@@ -65,7 +65,7 @@ sleep 10
 
 echo "raw performance test complete"
 
-sudo perf stat -B -o "$PERF_OUTPUT" -C 20-35,40-55 -e LLC-loads,LLC-load-misses,LLC-stores,cache-references,cache-misses,cycles,instructions &
+sudo perf stat -B -o "$PERF_OUTPUT" -C 20-40 -e LLC-loads,LLC-load-misses,LLC-stores,cache-references,cache-misses,cycles,instructions &
 ssh ubuntu@$prob_vm "sudo $comm_benchmark & sudo $comm_benchmark_1" 
 sudo kill -s SIGINT $(pidof perf)
 
@@ -92,7 +92,7 @@ ssh ubuntu@$prob_vm "sudo $comm_benchmark_1" >> "$OUTPUT_FILE2"
 echo "test finished"
 sleep 20
 
-sudo perf stat -B -o "$PERF_OUTPUT2" -C 20-35,40-55 -e LLC-loads,LLC-load-misses,LLC-stores,cache-references,cache-misses,cycles,instructions  &
+sudo perf stat -B -o "$PERF_OUTPUT2" -C 20-40 -e LLC-loads,LLC-load-misses,LLC-stores,cache-references,cache-misses,cycles,instructions  &
 ssh ubuntu@$prob_vm "sudo $comm_benchmark & sudo $comm_benchmark_1" 
 sudo kill -s SIGINT $(pidof perf)
 

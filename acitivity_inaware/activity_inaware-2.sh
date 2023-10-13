16d4f884109a2db6b7d642c097d665d2800418c6
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-10-13 17:09:09 -0400

    Update activity_inaware-2.sh

--- acitivity_inaware/activity_inaware-2.sh
+++ acitivity_inaware/activity_inaware-2.sh
@@ -4,7 +4,7 @@ compete_vm=$2
 benchmark_time=20
 latency_bench="cd /home/ubuntu/Workloads/tailbench-v0.9/img-dnn;time sudo bash run.sh"
 idler_bench="sudo bash /home/ubuntu/idle_load_generator/idler.sh"
-compete_bench="sudo sysbench --threads=16 --time=1000000 cpu run"
+compete_bench="sudo sysbench --threads=32 --time=1000000 cpu run"
 get_lat_val="cd /home/ubuntu/Workloads/tailbench-v0.9/utilities;sudo python parselats.py ../img-dnn/lats.bin"
 OUTPUT_FILE="./tests/acitivity_inaware-2$(date +%m%d%H%M).txt"
 

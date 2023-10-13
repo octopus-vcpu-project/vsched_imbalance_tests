e11f210e6ae43d820eb38e07b99773db7ce457e4
FarWrong <55039915+FarWrong@users.noreply.github.com>
2023-10-13 09:41:15 -0700

    Update activity_inaware-2 copy.sh

--- acitivity_inaware/activity_inaware-2 copy.sh
+++ acitivity_inaware/activity_inaware-2 copy.sh
@@ -3,6 +3,7 @@ prob_vm=$1
 compete_vm=$2
 benchmark_time=20
 latency_bench="sysbench --threads=32 --time=30 cpu run"
+compete_bench="sysbench --threads=32 --time=3000000 cpu run"
 OUTPUT_FILE="./tests/acitivity_inaware-2$(date +%m%d%H%M).txt"
 
 wake_and_pin_vm(){
@@ -15,28 +16,37 @@ wake_and_pin_vm(){
 }
 
 wake_and_pin_vm $prob_vm
+wake_and_pin_vm $compete_bench
 
 #Fetch VM PID and use that to fetch Cgroup title
 vm_pid=$(sudo grep pid /var/run/libvirt/qemu/$prob_vm.xml | awk -F "'" '{print $6}' | head -n 1)
 vm_cgroup_title=$(sudo cat /proc/$vm_pid/cgroup | awk -F "/" '{print $3}')
 
+c_vm_pid=$(sudo grep pid /var/run/libvirt/qemu/$prob_vm.xml | awk -F "'" '{print $6}' | head -n 1)
+c_vm_cgroup_title=$(sudo cat /proc/$vm_pid/cgroup | awk -F "/" '{print $3}')
 
+ssh ubuntu@$compete_bench "sudo $compete_bench"
 ssh ubuntu@$prob_vm "sudo $latency_bench"  >> "$OUTPUT_FILE" 
 
 for i in {0..31};do
     sudo echo 5000 10000 > /sys/fs/cgroup/machine.slice/$vm_cgroup_title/libvirt/vcpu$i/cpu.max
+    sudo echo 5000 10000 > /sys/fs/cgroup/machine.slice/$c_vm_cgroup_title/libvirt/vcpu$i/cpu.max
 done
 
 ssh ubuntu@$prob_vm "sudo $latency_bench"  >> "$OUTPUT_FILE" 
 
 for i in {0..31};do
     sudo echo 50000 100000 > /sys/fs/cgroup/machine.slice/$vm_cgroup_title/libvirt/vcpu$i/cpu.max
+    sudo echo 50000 100000 > /sys/fs/cgroup/machine.slice/$c_vm_cgroup_title/libvirt/vcpu$i/cpu.max
+
 done
 
 ssh ubuntu@$prob_vm "sudo $latency_bench"  >> "$OUTPUT_FILE" 
 
 for i in {0..31};do
     sudo echo 500000 1000000 > /sys/fs/cgroup/machine.slice/$vm_cgroup_title/libvirt/vcpu$i/cpu.max
+    sudo echo 500000 1000000 > /sys/fs/cgroup/machine.slice/$c_vm_cgroup_title/libvirt/vcpu$i/cpu.max
+
 done
 
 ssh ubuntu@$prob_vm "sudo $latency_bench"  >> "$OUTPUT_FILE" 

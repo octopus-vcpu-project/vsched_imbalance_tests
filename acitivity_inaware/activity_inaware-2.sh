6b920d87ae04befb1ef011213109043bd29d4b62
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-10-13 16:53:04 -0400

    asdf

--- acitivity_inaware/activity_inaware-2.sh
+++ acitivity_inaware/activity_inaware-2.sh
@@ -4,7 +4,7 @@ compete_vm=$2
 benchmark_time=20
 latency_bench="cd /home/ubuntu/Workloads/tailbench-v0.9/img-dnn;time sudo bash run.sh"
 idler_bench="sudo bash /home/ubuntu/idle_load_generator/idler.sh"
-compete_bench="sleep 50"
+compete_bench="sudo sysbench --threads=16 --time=1000000 cpu run"
 get_lat_val="cd /home/ubuntu/Workloads/tailbench-v0.9/utilities;sudo python parselats.py ../img-dnn/lats.bin"
 OUTPUT_FILE="./tests/acitivity_inaware-2$(date +%m%d%H%M).txt"
 
@@ -18,7 +18,7 @@ wake_and_pin_vm(){
 }
 
 wake_and_pin_vm $prob_vm
-#wake_and_pin_vm $compete_vm
+wake_and_pin_vm $compete_vm
 
 
 #Fetch VM PID and use that to fetch Cgroup title
@@ -30,44 +30,34 @@ c_vm_cgroup_title=$(sudo cat /proc/$c_vm_pid/cgroup | awk -F "/" '{print $3}')
 
 ssh ubuntu@$compete_vm "sudo killall a.out"
 ssh ubuntu@$prob_vm "sudo killall sysbench" 
-#ssh ubuntu@$compete_vm "sudo $compete_bench" &
+ssh ubuntu@$compete_vm "sudo $compete_bench" &
 ssh ubuntu@$prob_vm "$idler_bench" &
 sleep 10
 
 
-for i in {0..31};do
-    sudo echo 5000 10000 > /sys/fs/cgroup/machine.slice/$vm_cgroup_title/libvirt/vcpu$i/cpu.max
-    sudo echo 5000 10000 > /sys/fs/cgroup/machine.slice/$c_vm_cgroup_title/libvirt/vcpu$i/cpu.max
-done
+ssh ubuntu@$prob_vm "$latency_bench"  >> "$OUTPUT_FILE" 2>&1
+ssh ubuntu@$prob_vm "$get_lat_val"  >> "$OUTPUT_FILE" 2>&1
+
+sudo echo 18000000 > /sys/kernel/debug/sched/min_granularity_ns
+
+
 
 ssh ubuntu@$prob_vm "$latency_bench"  >> "$OUTPUT_FILE" 2>&1
 ssh ubuntu@$prob_vm "$get_lat_val"  >> "$OUTPUT_FILE" 2>&1
 
-for i in {0..31};do
-    sudo echo 10000 20000 > /sys/fs/cgroup/machine.slice/$vm_cgroup_title/libvirt/vcpu$i/cpu.max
-    sudo echo 10000 20000 > /sys/fs/cgroup/machine.slice/$c_vm_cgroup_title/libvirt/vcpu$i/cpu.max
-done
+sudo echo 9000000 > /sys/kernel/debug/sched/min_granularity_ns
 
-ssh ubuntu@$prob_vm "$latency_bench"  >> "$OUTPUT_FILE" 2>&1
-ssh ubuntu@$prob_vm "$get_lat_val"  >> "$OUTPUT_FILE" 2>&1
-for i in {0..31};do
-    sudo echo 20000 40000 > /sys/fs/cgroup/machine.slice/$vm_cgroup_title/libvirt/vcpu$i/cpu.max
-    sudo echo 20000 40000 > /sys/fs/cgroup/machine.slice/$c_vm_cgroup_title/libvirt/vcpu$i/cpu.max
-done
 
 ssh ubuntu@$prob_vm "$latency_bench"  >> "$OUTPUT_FILE" 2>&1
 ssh ubuntu@$prob_vm "$get_lat_val"  >> "$OUTPUT_FILE" 2>&1
 
+sudo echo 6000000 > /sys/kernel/debug/sched/min_granularity_ns
 
-for i in {0..31};do
-    sudo echo max 40000 > /sys/fs/cgroup/machine.slice/$vm_cgroup_title/libvirt/vcpu$i/cpu.max
-    sudo echo max 40000 > /sys/fs/cgroup/machine.slice/$c_vm_cgroup_title/libvirt/vcpu$i/cpu.max
-done
 
 ssh ubuntu@$prob_vm "$latency_bench"  >> "$OUTPUT_FILE" 2>&1
 ssh ubuntu@$prob_vm "$get_lat_val"  >> "$OUTPUT_FILE" 2>&1
 
-
+sudo echo 3000000 > /sys/kernel/debug/sched/min_granularity_ns
 
 sudo git add .;sudo git commit -m 'new';sudo git push
 

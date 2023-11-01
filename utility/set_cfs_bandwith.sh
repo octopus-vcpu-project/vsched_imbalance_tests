c95d439341fdf3517488ac1226b1c28c8e9bebda
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-11-01 15:28:44 -0400

    Update set_cfs_bandwith.sh

--- utility/set_cfs_bandwith.sh
+++ utility/set_cfs_bandwith.sh
@@ -5,8 +5,8 @@ total_period=$3
 vm_pid=$(sudo grep pid /var/run/libvirt/qemu/$prob_vm.xml | awk -F "'" '{print $6}' | head -n 1)
 vm_cgroup_title=$(sudo cat /proc/$vm_pid/cgroup | awk -F "/" '{print $3}')
 
-for i in {0..31};do
-    sudo echo $1 $2 > /sys/fs/cgroup/machine.slice/$vm_cgroup_title/libvirt/vcpu$i/cpu.max
+for i in {0..7};do
+    sudo echo $2 $3 > /sys/fs/cgroup/machine.slice/$vm_cgroup_title/libvirt/vcpu$i/cpu.max
 done
 echo "Set latency cfs to $1" 
 

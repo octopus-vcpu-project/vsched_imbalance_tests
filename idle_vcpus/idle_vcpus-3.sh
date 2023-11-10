f69a63bdd4235fb165e79313f8e76c234bb57c63
FarWrong <55039915+FarWrong@users.noreply.github.com>
2023-11-10 11:33:21 -0800

    Update idle_vcpus-3.sh

--- idle_vcpus/idle_vcpus-3.sh
+++ idle_vcpus/idle_vcpus-3.sh
@@ -6,7 +6,8 @@ latency_bench="cd /home/ubuntu/Workloads/tailbench-v0.9/img-dnn;time sudo bash r
 idler_bench="sudo bash /home/ubuntu/idle_load_generator/idler.sh"
 compete_bench="sudo sysbench --threads=32 --time=1000000 cpu run"
 get_lat_val="cd /home/ubuntu/Workloads/tailbench-v0.9/utilities;sudo python parselats-1.py ../img-dnn/lats.bin"
-OUTPUT_FILE="./tests/idle_vcpu-1$(date +%m%d%H%M).txt"
+OUTPUT_FILE="./tests/idle_vcpu-4-naive-$(date +%m%d%H%M).log"
+OUTPUT_FILE1="./tests/idle_vcpu-4-SMRT-$(date +%m%d%H%M).log"
 
 
 wake_and_pin_prob(){
@@ -31,11 +32,11 @@ ssh ubuntu@$compete_vm "sudo sysbench --time=30 --threads=16 cpu run"
 for i in {0..0};do
    echo "naive test" >> "$OUTPUT_FILE"
    ssh ubuntu@$prob_vm "cd Workloads;cd rt-app;sudo rt-app rtest.json" >> "$OUTPUT_FILE" 
-   scp ubuntu@$prob_vm:/home/ubuntu/Workloads/rt-app/rt-app-smrt-thread0-0.log ./tests/
+   scp ubuntu@$prob_vm:/home/ubuntu/Workloads/rt-app/test_logs/rt-app-smrt-thread0-0.log $OUTPUT_FILE
    sleep 3
    echo "non-naive test" >> "$OUTPUT_FILE"
    ssh ubuntu@$prob_vm "cd Workloads;cd rt-app;sudo rt-app rtest1.json" >> "$OUTPUT_FILE" 
-   scp ubuntu@$prob_vm:/home/ubuntu/Workloads/rt-app/rt-app3-naive-thread0-0.log ./tests/
+   scp ubuntu@$prob_vm:/home/ubuntu/Workloads/rt-app/test_logs/rt-app-naive-thread0-0.log $OUTPUT_FILE1
    sleep 4
 done
 sudo git add .;sudo git commit -m 'new';sudo git push

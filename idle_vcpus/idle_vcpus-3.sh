bad06ebd3282b6fbe7c6cd4e71a0b56a2ceeb920
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-11-13 16:41:25 -0500

    Update idle_vcpus-3.sh

--- idle_vcpus/idle_vcpus-3.sh
+++ idle_vcpus/idle_vcpus-3.sh
@@ -36,6 +36,9 @@ run_test_series(){
     echo "200000(standard) halt polling">> "$OUTPUT_FILE" 
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$benchmark;sudo bash run.sh"
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/utilities;sudo python parselats-1.py ../$benchmark/lats.bin" >> "$OUTPUT_FILE" 
+    echo "Clumped(0-4)">> "$OUTPUT_FILE" 
+    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$benchmark;sudo taskset -c 0-4 bash run.sh"
+    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/utilities;sudo python parselats-1.py ../$benchmark/lats.bin" >> "$OUTPUT_FILE" 
 
     ssh ubuntu@$prob_vm "$idler_bench" &
     echo "W/ Idle Workload(naive)">> "$OUTPUT_FILE" 

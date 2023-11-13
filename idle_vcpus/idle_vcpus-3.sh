8464e4d69a8b85e2b75fb1466bb6d283c994b992
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-11-13 17:38:10 -0500

    Update idle_vcpus-3.sh

--- idle_vcpus/idle_vcpus-3.sh
+++ idle_vcpus/idle_vcpus-3.sh
@@ -29,13 +29,16 @@ run_test_series(){
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$benchmark;sudo bash run.sh"
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/utilities;sudo python parselats-1.py ../$benchmark/lats.bin" >> "$OUTPUT_FILE" 
     sudo tee /sys/module/kvm/parameters/halt_poll_ns <<< 200000
+    sleep 4
     echo "200000(standard) halt polling">> "$OUTPUT_FILE" 
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$benchmark;sudo bash run.sh"
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/utilities;sudo python parselats-1.py ../$benchmark/lats.bin" >> "$OUTPUT_FILE" 
     sudo tee /sys/module/kvm/parameters/halt_poll_ns <<< 2000000
+    sleep 4
     echo "2000000(high) halt polling" >> "$OUTPUT_FILE" 
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$benchmark;sudo bash run.sh"
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/utilities;sudo python parselats-1.py ../$benchmark/lats.bin" >> "$OUTPUT_FILE" 
+    sleep 4
     sudo tee /sys/module/kvm/parameters/halt_poll_ns <<< 200000
     echo "Clumped(0-7)">> "$OUTPUT_FILE" 
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$benchmark;sudo taskset -c 0-7 bash run.sh"

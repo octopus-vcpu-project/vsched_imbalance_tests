f78ff7bc122df9632389008109c649aba39c33b1
edward <eguo3@pride.hofstra.edu>
2023-11-13 15:21:11 -0500

    new

--- utility/set_cfs_bandwith.sh
+++ utility/set_cfs_bandwith.sh
@@ -5,7 +5,7 @@ total_period=$3
 vm_pid=$(sudo grep pid /var/run/libvirt/qemu/$prob_vm.xml | awk -F "'" '{print $6}' | head -n 1)
 vm_cgroup_title=$(sudo cat /proc/$vm_pid/cgroup | awk -F "/" '{print $3}')
 
-for i in {0..7};do
+for i in {1..7};do
     sudo echo $2 $3 > /sys/fs/cgroup/machine.slice/$vm_cgroup_title/libvirt/vcpu$i/cpu.max
 done
 echo "Set latency cfs to $1" 

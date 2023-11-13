a939c4d09aa074f4c4fb6db1ce6a5cb2ca4bb006
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-11-13 17:01:41 -0500

    Update idle_vcpus-3.sh

--- idle_vcpus/idle_vcpus-3.sh
+++ idle_vcpus/idle_vcpus-3.sh
@@ -3,7 +3,7 @@ prob_vm=$1
 compete_vm=$2
 benchmark_time=20
 latency_bench="cd /home/ubuntu/Workloads/Tailbench/tailbench;time sudo bash run.sh"
-idler_bench="sudo taskset -c 0-7 bash /home/ubuntu/idle_load_generator/idler.sh"
+idler_bench="sudo taskset -c 0-15 bash /home/ubuntu/idle_load_generator/idler.sh"
 compete_bench="sudo sysbench --threads=32 --time=1000000 cpu run"
 get_lat_val="cd /home/ubuntu/Workloads/tailbench/utilities;sudo python parselats-1.py ../img-dnn/lats.bin"
 OUTPUT_FILE="./tests/idle_vcpu-4-naive-$(date +%m%d%H%M).log"
@@ -13,8 +13,8 @@ OUTPUT_FILE2="./tests/idle_vcpu-4-dflt-$(date +%m%d%H%M).log"
 
 wake_and_pin_prob(){
     select_vm=$1
-    sudo bash ../utility/cleanon_startup.sh $select_vm 16
-    for i in {0..15};do
+    sudo bash ../utility/cleanon_startup.sh $select_vm 32
+    for i in {0..31};do
         sudo virsh vcpupin $select_vm $i $((i+20))
     done
     sleep 2
@@ -36,7 +36,7 @@ run_test_series(){
     echo "200000(standard) halt polling">> "$OUTPUT_FILE" 
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$benchmark;sudo bash run.sh"
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/utilities;sudo python parselats-1.py ../$benchmark/lats.bin" >> "$OUTPUT_FILE" 
-    echo "Clumped(0-4)">> "$OUTPUT_FILE" 
+    echo "Clumped(0-7)">> "$OUTPUT_FILE" 
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$benchmark;sudo taskset -c 0-7 bash run.sh"
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/utilities;sudo python parselats-1.py ../$benchmark/lats.bin" >> "$OUTPUT_FILE" 
 
@@ -46,7 +46,7 @@ run_test_series(){
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/utilities;sudo python parselats-1.py ../$benchmark/lats.bin" >> "$OUTPUT_FILE" 
 
     echo "W/ Idle Workload(smart)">> "$OUTPUT_FILE" 
-    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$benchmark;sudo taskset -c 0-7 bash run.sh"
+    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$benchmark;sudo taskset -c 0-15 bash run.sh"
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/utilities;sudo python parselats-1.py ../$benchmark/lats.bin" >> "$OUTPUT_FILE" 
 }
 

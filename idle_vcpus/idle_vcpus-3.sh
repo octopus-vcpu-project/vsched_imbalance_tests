dcbc0e4494e87119762ed3eb356ca7d0b399a992
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-11-13 15:19:50 -0500

    Update idle_vcpus-3.sh

--- idle_vcpus/idle_vcpus-3.sh
+++ idle_vcpus/idle_vcpus-3.sh
@@ -2,10 +2,10 @@
 prob_vm=$1
 compete_vm=$2
 benchmark_time=20
-latency_bench="cd /home/ubuntu/Workloads/tailbench-v0.9/img-dnn;time sudo bash run.sh"
-idler_bench="sudo bash /home/ubuntu/idle_load_generator/idler.sh"
+latency_bench="cd /home/ubuntu/Workloads/Tailbench/tailbench;time sudo bash run.sh"
+idler_bench="sudo taskset -c 0-15 bash /home/ubuntu/idle_load_generator/idler.sh"
 compete_bench="sudo sysbench --threads=32 --time=1000000 cpu run"
-get_lat_val="cd /home/ubuntu/Workloads/tailbench-v0.9/utilities;sudo python parselats-1.py ../img-dnn/lats.bin"
+get_lat_val="cd /home/ubuntu/Workloads/tailbench/utilities;sudo python parselats-1.py ../img-dnn/lats.bin"
 OUTPUT_FILE="./tests/idle_vcpu-4-naive-$(date +%m%d%H%M).log"
 OUTPUT_FILE1="./tests/idle_vcpu-4-SMRT-$(date +%m%d%H%M).log"
 OUTPUT_FILE2="./tests/idle_vcpu-4-dflt-$(date +%m%d%H%M).log"
@@ -21,32 +21,43 @@ wake_and_pin_prob(){
 }
 
 
+run_test_series(){
+    benchmark=$1
+    ssh ubuntu@$prob_vm "sudo killall a.out"
+    sudo tee /sys/module/kvm/parameters <<< 2000000
+    echo "2000000(high) halt polling" >> "$OUTPUT_FILE" 
+    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$benchmark;sudo bash run.sh"
+    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/tailbench/utilities;sudo python parselats-1.py ../$benchmark/lats.bin" >> "$OUTPUT_FILE" 
+    sudo tee /sys/module/kvm/parameters <<< 0
+    
+    echo "0(no) halt polling">> "$OUTPUT_FILE" 
+    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$benchmark;sudo bash run.sh"
+    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/tailbench/utilities;sudo python parselats-1.py ../$benchmark/lats.bin" >> "$OUTPUT_FILE" 
+
+    sudo tee /sys/module/kvm/parameters <<< 200000
+    echo "200000(standard) halt polling">> "$OUTPUT_FILE" 
+    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$benchmark;sudo bash run.sh"
+    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/tailbench/utilities;sudo python parselats-1.py ../$benchmark/lats.bin" >> "$OUTPUT_FILE" 
+
+    ssh ubuntu@$prob_vm "$idler_bench"
+    echo "W/ Idle Workload(naive)">> "$OUTPUT_FILE" 
+    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$benchmark;sudo bash run.sh"
+    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/tailbench/utilities;sudo python parselats-1.py ../$benchmark/lats.bin" >> "$OUTPUT_FILE" 
+
+    echo "W/ Idle Workload(smart)">> "$OUTPUT_FILE" 
+    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$benchmark;sudo taskset -c 0-16 bash run.sh"
+    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/tailbench/utilities;sudo python parselats-1.py ../$benchmark/lats.bin" >> "$OUTPUT_FILE" 
+}
 
 
 vm_pid=$(sudo grep pid /var/run/libvirt/qemu/$prob_vm.xml | awk -F "'" '{print $6}' | head -n 1)
 
-
+virsh shutdown $prob_vm
+virsh shutdown $compete_vm
 wake_and_pin_prob $prob_vm
-#wake_and_pin_prob $compete_vm
-#ssh ubuntu@$compete_vm "sudo sysbench --time=90000000 --threads=32 cpu run"  &
-sudo tee /sys/module/kvm/parameters/halt_poll_ns <<< 0
-for i in {0..0};do
-       
-
-   echo "naive test" >> "$OUTPUT_FILE"
-   ssh ubuntu@$prob_vm "cd Workloads;cd rt-app;sudo rt-app rtest1.json" >> "$OUTPUT_FILE" 
-   scp ubuntu@$prob_vm:/home/ubuntu/Workloads/rt-app/test_logs/rt-app-naive-thread0-0.log $OUTPUT_FILE
-   sleep 3
-done
-sudo tee /sys/module/kvm/parameters/halt_poll_ns <<< 2000000
-ssh ubuntu@$prob_vm "cd Workloads;cd rt-app;sudo rt-app rtest1.json"
-scp ubuntu@$prob_vm:/home/ubuntu/Workloads/rt-app/test_logs/rt-app-naive-thread0-0.log $OUTPUT_FILE1
-sleep 3
-sudo tee /sys/module/kvm/parameters/halt_poll_ns <<< 200000
-ssh ubuntu@$prob_vm "cd Workloads;cd rt-app;sudo rt-app rtest1.json"
-scp ubuntu@$prob_vm:/home/ubuntu/Workloads/rt-app/test_logs/rt-app-naive-thread0-0.log $OUTPUT_FILE2
-sleep 3
-ssh ubuntu@$compete_vm "killall sysbench" 
+wake_and_pin_prob $compete_vm
+ssh ubuntu@$compete_vm "sudo sysbench --time=90000000 --threads=32 cpu run"  &
+run_test_series "img-dnn"
 sudo git add .;sudo git commit -m 'new';sudo git push
 
 

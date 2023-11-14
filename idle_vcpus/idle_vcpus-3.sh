e195d4a8717607332ad3f749edeb940e93671a26
FarWrong <55039915+FarWrong@users.noreply.github.com>
2023-11-13 22:14:31 -0500

    Update idle_vcpus-3.sh

--- idle_vcpus/idle_vcpus-3.sh
+++ idle_vcpus/idle_vcpus-3.sh
@@ -42,6 +42,10 @@ run_test_series(){
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/utilities;sudo python parselats-1.py ../$benchmark/lats.bin" >> "$OUTPUT_FILE" 
     sleep 4
     sudo tee /sys/module/kvm/parameters/halt_poll_ns <<< 200000
+    echo "Clumped(0-15)">> "$OUTPUT_FILE" 
+    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$benchmark;sudo taskset -c 0-15 bash run.sh"
+    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/utilities;sudo python parselats-1.py ../$benchmark/lats.bin" >> "$OUTPUT_FILE" 
+    
     echo "Clumped(0-7)">> "$OUTPUT_FILE" 
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$benchmark;sudo taskset -c 0-7 bash run.sh"
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/utilities;sudo python parselats-1.py ../$benchmark/lats.bin" >> "$OUTPUT_FILE" 

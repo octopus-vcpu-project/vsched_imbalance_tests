5010103fd58a2d7f08a90bf179c2489208e29622
edward <eguo3@pride.hofstra.edu>
2024-01-19 11:42:34 -0500

    new

--- cpc_aware/sym-test.sh
+++ cpc_aware/sym-test.sh
@@ -1,7 +1,8 @@
-prob_vm=$1
-compete_vm=$2
-cpu_benchmark="sysbench --threads=4 --time=10 cpu run"
-compete_benchmark="sysbench --threads=16 --time=1000000 cpu run"
+prob_vm=$1 
+compete_vm=$2 
+cpu_benchmark="sysbench --threads=4 --time=10 cpu run" 
+compete_benchmark="sysbench --threads=16 --time=1000000 cpu run" 
+cpu_benchmark_pinned="taskset -c 12-15 sysbench --threads=4 --time=10 cpu run"
 sudo bash ../utility/cleanon_startup.sh $prob_vm 16 $compete_vm 16
 
 for i in {0..15};do
@@ -9,10 +10,15 @@ for i in {0..15};do
     sudo virsh vcpupin $compete_vm $i $i
 done
 
+
+
+
+
+
 echo "Finished Pinning"
 
-ssh ubuntu@$prob_vm "sudo killall sysbench" 
-ssh ubuntu@$compete_vm "sudo killall sysbench"
+#ssh ubuntu@$prob_vm "sudo killall sysbench" 
+#ssh ubuntu@$compete_vm "sudo killall sysbench"
 ssh ubuntu@$compete_vm "sudo $compete_benchmark" &
 
 #topology naive testing
@@ -21,4 +27,13 @@ for i in {0..50};do
     ssh ubuntu@$prob_vm "sudo $cpu_benchmark" >> "$OUTPUT_FILE"
 done
 touch $OUTPUT_FILE
+
+#topology naive testing
+OUTPUT_FILE="./test/sym-opt$(date +%m%d%H%M).txt"
+for i in {0..50};do
+    ssh ubuntu@$prob_vm "sudo $cpu_benchmark_pinned" >> "$OUTPUT_FILE"
+done
+touch $OUTPUT_FILE
 echo "test complete"
+
+

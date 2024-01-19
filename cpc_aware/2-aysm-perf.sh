7abd11dc07dd68d784331c5e39f021137894d030
edward <eguo3@pride.hofstra.edu>
2024-01-19 12:35:33 -0500

    new

--- cpc_aware/2-aysm-perf.sh
+++ cpc_aware/2-aysm-perf.sh
@@ -10,38 +10,60 @@ Runtime:1000s'
 
 prob_vm=$1
 compete_vm=$2
+compete_vm2=$3
+compete_vm3=$4
 cpu_benchmark="sysbench --threads=4 --time=10 cpu run"
-cpu_benchmark_opt="taskset -c 12-15 sysbench --threads=4 --time=10 cpu run"
-compete_benchmark="sysbench --threads=52 --time=2000 cpu run"
-sudo bash ../utility/cleanon_startup.sh $prob_vm 16 $compete_vm 52
+compete_benchmark="sysbench --threads=32 --time=5000 cpu run"
+sudo bash ../utility/cleanon_startup.sh $prob_vm 16 $compete_vm 32 $compete_vm2 32 $compete_vm3 32
 
-for i in {0..15};do
+for i in {0..11};do
     sudo virsh vcpupin $prob_vm $i $i
+    sudo virsh vcpupin $compete_vm $i $((i+40))
+    sudo virsh vcpupin $compete_vm2 $i $i
+    sudo virsh vcpupin $compete_vm3 $i $i
+done
+
+for i in {12..15};do
     sudo virsh vcpupin $compete_vm $i $i
+    sudo virsh vcpupin $prob_vm $i $i
+    sudo virsh vcpupin $compete_vm2 $i $((i%12))
+    sudo virsh vcpupin $compete_vm3 $i $((i%12))
 done
 
-for i in {16..51};do
-    sudo virsh vcpupin $compete_vm $i $(( i%12 ))
+for i in {16..23};do
+    sudo virsh vcpupin $compete_vm2 $i $(( i%12 ))
+    sudo virsh vcpupin $compete_vm3 $i $(( i%12 ))
+    sudo virsh vcpupin $compete_vm $i $((i+40))
 done
 
+for i in {24..31};do
+    sudo virsh vcpupin $compete_vm2 $i $(( i+40 ))
+    sudo virsh vcpupin $compete_vm3 $i $(( i+40 ))
+    sudo virsh vcpupin $compete_vm $i $((i+40))
+done
 
 echo "Finished Pinning"
 
 ssh ubuntu@$prob_vm "sudo killall sysbench" 
 ssh ubuntu@$compete_vm "sudo killall sysbench"
+ssh ubuntu@$compete_vm2 "sudo killall sysbench"
+ssh ubuntu@$compete_vm3 "sudo killall sysbench"
+
 ssh ubuntu@$compete_vm "sudo $compete_benchmark" &
+ssh ubuntu@$compete_vm2 "sudo $compete_benchmark" &
+ssh ubuntu@$compete_vm3 "sudo $compete_benchmark" &
+ssh ubuntu@$prob_vm "sudo $cpu_benchmark" &
 
-#topology naive testing
-OUTPUT_FILE="./tests/2-asym-naive-$(date +%m%d%H%M).txt"
+OUTPUT_FILE="./tests/1-asym-perf-naive-$(date +%m%d%H%M).txt"
 for i in {0..50};do
-    ssh ubuntu@$prob_vm "sudo $cpu_benchmark" >> "$OUTPUT_FILE"
+	ssh ubuntu@$prob_vm "sudo $cpu_benchmark" >> "$OUTPUT_FILE"
 done
-
-#topology opt testing
-OUTPUT_FILE="./tests/2-asym-opt-$(date +%m%d%H%M).txt"
-for i in {0..50};do
-    ssh ubuntu@$prob_vm "sudo $cpu_benchmark_opt" >> "$OUTPUT_FILE"
-done
-
 touch $OUTPUT_FILE
-echo "Test cpc_aware 2-asym-perf is complete"
+cpu_benchmark="taskset -c 12-15 sysbench --threads=4 --time=10 cpu run"
+OUTPUT_FILE="./tests/1-asym-perf-opt-$(date +%m%d%H%M).txt"
+for i in {0..50};do
+        ssh ubuntu@$prob_vm "sudo $cpu_benchmark" >> "$OUTPUT_FILE"
+done
+touch $OUTPUT_FILE
+
+echo "Test cpc_aware 1-asym-plc is complete"

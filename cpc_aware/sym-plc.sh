06e3b1c2c566958f1f87386a27adf74a3d5a1a3d
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2024-01-20 22:43:20 -0500

    Update sym-plc.sh

--- cpc_aware/sym-plc.sh
+++ cpc_aware/sym-plc.sh
@@ -18,7 +18,7 @@ ssh ubuntu@$compete_vm "sudo killall sysbench"
 ssh ubuntu@$compete_vm "sudo $compete_benchmark" &
 #topology naive testing
 OUTPUT_FILE="./test/sym-plc$(date +%m%d%H%M).txt"
-ssh ubuntu@$prob_vm "sudo python /home/ubuntu/bpftrace/bcc/tools/runqlen.py -C">> "$OUTPUT_FILE" &
+ssh ubuntu@$prob_vm "sudo python3 /home/ubuntu/bpftrace/bcc/tools/runqlen.py -C">> "$OUTPUT_FILE" &
 ssh ubuntu@$prob_vm "sudo $cpu_benchmark" &
 sleep $benchmark_time
 ssh ubuntu@$prob_vm "sudo kill -s SIGINT \$(pidof python)"
@@ -29,7 +29,7 @@ ssh ubuntu@$prob_vm "nohup sudo /home/ubuntu/cpu_profiler/joe.out -v -i 500 -s 1
 OUTPUT_FILE="./test/sym-plc-smrt$(date +%m%d%H%M).txt"
 
 
-ssh ubuntu@$prob_vm "sudo python /home/ubuntu/bpftrace/bcc/tools/runqlen.py -C">> "$OUTPUT_FILE" &
+ssh ubuntu@$prob_vm "sudo python3 /home/ubuntu/bpftrace/bcc/tools/runqlen.py -C">> "$OUTPUT_FILE" &
 ssh ubuntu@$prob_vm "sudo $cpu_benchmark" &
 sleep $benchmark_time
 ssh ubuntu@$prob_vm "sudo kill -s SIGINT \$(pidof python)"

b339819bd8ee40fbf5605a8e6e3c89870ebf9632
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2024-01-20 22:46:21 -0500

    Update sym-plc.sh

--- cpc_aware/sym-plc.sh
+++ cpc_aware/sym-plc.sh
@@ -1,6 +1,6 @@
 prob_vm=$1
 compete_vm=$2
-benchmark_time=200
+benchmark_time=100
 cpu_benchmark="sysbench --threads=4 --time=600 cpu run"
 compete_benchmark="sysbench --threads=16 --time=200000 cpu run"
 sudo bash ../utility/cleanon_startup.sh $prob_vm 16 $compete_vm 16
@@ -14,21 +14,21 @@ echo "Finished Pinning"
 
 ssh ubuntu@$prob_vm "sudo killall sysbench" 
 ssh ubuntu@$prob_vm "sudo killall joe.out"
+ssh ubuntu@$prob_vm "sudo $compete_benchmark" &
 ssh ubuntu@$compete_vm "sudo killall sysbench"
-ssh ubuntu@$compete_vm "sudo $compete_benchmark" &
 #topology naive testing
 OUTPUT_FILE="./test/sym-plc$(date +%m%d%H%M).txt"
-ssh ubuntu@$prob_vm "sudo python3 /home/ubuntu/bpftrace/bcc/tools/runqlen.py -C">> "$OUTPUT_FILE" &
-ssh ubuntu@$prob_vm "sudo $cpu_benchmark" &
+ssh ubuntu@$compete_vm "sudo python3 /home/ubuntu/bpftrace/bcc/tools/runqlen.py -C">> "$OUTPUT_FILE" &
+ssh ubuntu@$compete_vm "sudo $cpu_benchmark" &
 sleep $benchmark_time
-ssh ubuntu@$prob_vm "sudo kill -s SIGINT \$(pidof python)"
-ssh ubuntu@$prob_vm "sudo killall sysbench"
+ssh ubuntu@$compete_vm "sudo kill -s SIGINT \$(pidof python)"
 
+ssh ubuntu@$compete_vm "sudo killall sysbench"
+ssh ubuntu@$prob_vm "sudo killall sysbench"
+ssh ubuntu@$compete_vm "sudo $compete_benchmark" &
 ssh ubuntu@$prob_vm "sudo bash /home/ubuntu/cpu_profiler/setup_vcapacity.sh"
 ssh ubuntu@$prob_vm "nohup sudo /home/ubuntu/cpu_profiler/joe.out -v -i 500 -s 10000 &  " & 
 OUTPUT_FILE="./test/sym-plc-smrt$(date +%m%d%H%M).txt"
-
-
 ssh ubuntu@$prob_vm "sudo python3 /home/ubuntu/bpftrace/bcc/tools/runqlen.py -C">> "$OUTPUT_FILE" &
 ssh ubuntu@$prob_vm "sudo $cpu_benchmark" &
 sleep $benchmark_time

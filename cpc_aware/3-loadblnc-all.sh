cb6166417a3a951c9a82fee180e5fc9db3931015
FarWrong <55039915+FarWrong@users.noreply.github.com>
2024-01-22 19:29:21 -0800

    Update 3-loadblnc-all.sh

--- cpc_aware/3-loadblnc-all.sh
+++ cpc_aware/3-loadblnc-all.sh
@@ -9,7 +9,7 @@ OUTPUT_FILE2="./test/2-dis-hrd$(date +%m%d%H%M).txt"
 prob_vm=$1
 runtime=$2
 period=$3
-cpu_benchmark="sysbench --threads=64 --time=90 cpu run"
+cpu_benchmark="sysbench --threads=64 --time=120 cpu run"
 
 sudo bash ../utility/cleanon_startup.sh $prob_vm 32
 #Fetch VM PID and use that to fetch Cgroup title
@@ -96,7 +96,7 @@ done
 
 OUTPUT_FILE="./test/unf-asym-nve-$(date +%m%d%H%M).txt"
 for i in {0..30};do
-    sleep 2
+    sleep 3
     output_thread_specific_vruntimes "${thread_ids[@]}"
 done
 
@@ -115,7 +115,7 @@ OUTPUT_FILE="./test/unf-asym-pin-$(date +%m%d%H%M).txt"
 
 pin_threads_smartly "${mread_ids[@]}"
 for i in {0..30};do
-    sleep 2
+    sleep 3
     output_thread_specific_vruntimes "${mread_ids[@]}"
 done
 echo "unf-asym-pin test complete"
@@ -136,7 +136,7 @@ for tid in $(ssh ubuntu@$prob_vm "ls /proc/$sysbench_pid/task");do
     new_iterator=$((new_iterator + 1))
 done
 for i in {0..30};do
-    sleep 2
+    sleep 3
     output_thread_specific_vruntimes "${smrt_thread_ids[@]}"
 done
 wipe_clean $prob_vm

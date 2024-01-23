1c468dfe0247c7bf04b5a294a54cc5bf4813e9ef
FarWrong <55039915+FarWrong@users.noreply.github.com>
2024-01-23 09:43:09 -0800

    Update 3-loadblnc-all-local.sh

--- cpc_aware/3-loadblnc-all-local.sh
+++ cpc_aware/3-loadblnc-all-local.sh
@@ -7,29 +7,32 @@ OUTPUT_FILE="./test/unfair$(date +%m%d%H%M).txt"
 OUTPUT_FILE2="./test/2-dis-hrd$(date +%m%d%H%M).txt"
 
 prob_vm=$1
-runtime=$2
-period=$3
-cpu_benchmark="sysbench --threads=64 --time=40 cpu run"
+compete_vm=$2
+cpu_benchmark="sysbench --threads=32 --time=100 cpu run"
+compete_benchmark="sysbench --threads=32 --time=10000 cpu run"
 
-sudo bash ../utility/cleanon_startup.sh $prob_vm 32
+sudo bash ../utility/cleanon_startup.sh $prob_vm 16
+sudo bash ../utility/cleanon_startup.sh $compete_vm 32
 #Fetch VM PID and use that to fetch Cgroup title
 vm_pid=$(sudo grep pid /var/run/libvirt/qemu/$prob_vm.xml | awk -F "'" '{print $6}' | head -n 1)
 vm_cgroup_title=$(sudo cat /proc/$vm_pid/cgroup | awk -F "/" '{print $3}')
 #PIN VCPUS and limit CPU usage using CGROUP
 
 ssh ubuntu@$prob_vm "sudo killall sysbench" 
-for i in {0..31};do
-    sudo virsh vcpupin $prob_vm $i $((i + 32))
-    sudo echo $runtime $period > /sys/fs/cgroup/machine.slice/$vm_cgroup_title/libvirt/vcpu$i/cpu.max
+for i in {0..15};do
+    sudo virsh vcpupin $prob_vm $i $((i + 20))
 done
 
+for i in {0..31};do
+    sudo virsh vcpupin $compete_vm $i $(((i%16) + 20))
+done
 
+ssh ubuntu@$compete_vm "sudo $compete_benchmark" 
 wipe_clean(){
     local local_prob_vm=$1
     ssh ubuntu@"$local_prob_vm" "sudo killall sysbench"
     ssh ubuntu@"$local_prob_vm" "sudo killall joe.out"
 }
-
 
 
 pin_threads_smartly(){
@@ -41,12 +44,12 @@ pin_threads_smartly(){
         if [ $tid -eq $sysbench_pid ]; then
             continue
         fi 
-        if [ $iterator -lt 16 ]; then
-            pin_location=$iterator
-        elif [ $iterator -lt 64 ]; then
-            pin_location=$((iterator % 16 + 16))
+        if [ $iterator -lt 8 ]; then
+            pin_location=$((iterator))
+        elif [ $iterator -lt 32 ]; then
+            pin_location=$((iterator % 8 + 8))
         fi
-        command_str+="taskset -cp $pin_location $tid; "
+        command_str+="taskset -cp $pin_location $tid;"
         iterator=$((iterator + 1))
     done
     ssh ubuntu@"$prob_vm" "$command_str" >> "$OUTPUT_FILE"
@@ -82,55 +85,63 @@ echo "unf-sym-nve test complete"
 comm
 #this test is where half the cores are much weaker, assymetrically competed for.
 
-for i in {0..15};do
-    sudo echo $((runtime/3)) $period > /sys/fs/cgroup/machine.slice/$vm_cgroup_title/libvirt/vcpu$i/cpu.max
-done
-OUTPUT_FILE="./test/unf-asym-nve-$(date +%m%d%H%M).txt"
+
 
 wipe_clean $prob_vm
 ssh ubuntu@$prob_vm "$cpu_benchmark"    &
-sleep 2
+sleep 1
 sysbench_pid=$(ssh ubuntu@$prob_vm "pidof sysbench")
-ssh ubuntu@$prob_vm "sudo /home/ubuntu/vsched/tools/perf/perf stat -a --per-thread -o /home/ubuntu/testout.txt -e task-clock -p $sysbench_pid -I 1000"  &
-sleep 38
-ssh ubuntu@$prob_vm "sudo kill -s SIGINT $(pidof perf)"
-scp ubuntu@$prob_vm:/home/ubuntu/testout.txt ./$OUTPUT_FILE
+declare -a thread_ids
+new_iterator=0
+for tid in $(ssh ubuntu@$prob_vm "ls /proc/$sysbench_pid/task");do
+    thread_ids+=($tid)
+    new_iterator=$((new_iterator + 1))
+done
+OUTPUT_FILE="./test/unf-asym-nve-$(date +%m%d%H%M).txt"
+for i in {0..25};do
+    sleep 4
+    output_thread_specific_vruntimes "${thread_ids[@]}"
+done
 
 echo "unf-asym-nve test complete"
+
 wipe_clean $prob_vm
-OUTPUT_FILE="./test/unf-asym-pin-$(date +%m%d%H%M).txt"
-ssh ubuntu@$prob_vm "$cpu_benchmark" &   
-sleep 2
+ssh ubuntu@$prob_vm "$cpu_benchmark"    &
+sleep 1
 sysbench_pid=$(ssh ubuntu@$prob_vm "pidof sysbench")
-ssh ubuntu@$prob_vm "sudo /home/ubuntu/vsched/tools/perf/perf stat -a --per-thread -o testout.txt -e task-clock -p $sysbench_pid -I 1000"  &
 declare -a mread_ids
 for tid in $(ssh ubuntu@$prob_vm "ls /proc/$sysbench_pid/task");do
     mread_ids+=($tid)
     new_iterator=$((new_iterator + 1))
 done
+OUTPUT_FILE="./test/unf-asym-pin-$(date +%m%d%H%M).txt"
+
 pin_threads_smartly "${mread_ids[@]}"
-echo "Finished pinning!"
-sleep 38
-ssh ubuntu@$prob_vm "sudo kill -s SIGINT $(pidof perf)"
-scp ubuntu@$prob_vm:/home/ubuntu/testout.txt ./$OUTPUT_FILE
+for i in {0..25};do
+    sleep 3
+    output_thread_specific_vruntimes "${mread_ids[@]}"
+done
+echo "unf-asym-pin test complete"
+
 
 OUTPUT_FILE="./test/unf-asym-smrt-$(date +%m%d%H%M).txt"
 wipe_clean $prob_vm
 ssh ubuntu@$prob_vm "sudo bash /home/ubuntu/cpu_profiler/setup_vcapacity.sh"
 ssh ubuntu@$prob_vm "nohup sudo /home/ubuntu/cpu_profiler/joe.out -v -i 500 -s 15000 &  " & 
-ssh ubuntu@$prob_vm "$cpu_benchmark"   &
-sleep 2
-ssh ubuntu@$prob_vm "sudo /home/ubuntu/vsched/tools/perf/perf stat -a --per-thread -o testout.txt -e task-clock -p $sysbench_pid -I 1000" &
-sleep 40
-ssh ubuntu@$prob_vm "sudo kill -s SIGINT $(pidof perf)"
-scp ubuntu@$prob_vm:/home/ubuntu/testout.txt ./$OUTPUT_FILE
 
-<<comm
-for i in {0..30};do
+ssh ubuntu@$prob_vm "$cpu_benchmark"    &
+sleep 1
+sysbench_pid=$(ssh ubuntu@$prob_vm "pidof sysbench")
+declare -a smrt_thread_ids
+new_iterator=0
+for tid in $(ssh ubuntu@$prob_vm "ls /proc/$sysbench_pid/task");do
+    smrt_thread_ids+=($tid)
+    new_iterator=$((new_iterator + 1))
+done
+for i in {0..25};do
     sleep 3
     output_thread_specific_vruntimes "${smrt_thread_ids[@]}"
 done
-comm
 wipe_clean $prob_vm
 
 

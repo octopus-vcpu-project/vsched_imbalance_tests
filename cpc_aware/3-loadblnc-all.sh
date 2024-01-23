8ad3797ee49c276e89966ca7a4b865715f7a0d78
FarWrong <55039915+FarWrong@users.noreply.github.com>
2024-01-23 12:10:09 -0800

    Update 3-loadblnc-all.sh

--- cpc_aware/3-loadblnc-all.sh
+++ cpc_aware/3-loadblnc-all.sh
@@ -82,9 +82,9 @@ comm
 #this test is where half the cores are much weaker, assymetrically competed for.
 
 
-for i in {0..8};do
-    sudo echo $((runtime*10))  $((period*10)) > /sys/fs/cgroup/machine.slice/$vm_cgroup_title/libvirt/vcpu$i/cpu.max
-done
+#for i in {0..8};do
+#    sudo echo $((runtime*10))  $((period*10)) > /sys/fs/cgroup/machine.slice/$vm_cgroup_title/libvirt/vcpu$i/cpu.max
+#done
 
 
 wipe_clean $prob_vm

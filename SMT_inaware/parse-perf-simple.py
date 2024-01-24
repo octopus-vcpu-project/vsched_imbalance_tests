9e0bbb16df7311ef5d392aa381f95a6a7dc349d6
FarWrong <55039915+FarWrong@users.noreply.github.com>
2024-01-24 11:27:58 -0500

    asdf

---
+++ SMT_inaware/parse-perf-simple.py
@@ -0,0 +1,76 @@
+import glob
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Find all files that match the 'sym-plc*' pattern
+def get_average(list):
+    return sum(list)/len(list)
+def process_file(n,s):
+    files = glob.glob(s)
+    files.sort(reverse=True)
+
+    if len(files) >= n:
+        file_to_read = files[n-1]
+        cpu_sysbench_counts = []
+        with open(file_to_read, 'r') as f:
+            lines = f.readlines()
+            current_cpu = -1
+            for line in lines:
+                if "total number of events" in line:
+                    cpu_amount = int(line.split()[-1])
+                    cpu_sysbench_counts.append(cpu_amount)
+                    print(cpu_amount)
+
+        return get_average(cpu_sysbench_counts)
+    else:
+        print(f"No matching file found for index {n}")
+        return []
+
+
+
+
+def plot_grouped_data_with_legends(data_dict, name_parameters):
+    """
+    Plots a grouped bar chart for each group in the data dictionary with 
+    separate bars for each category in name_parameters, using different patterns for each group.
+
+    :param data_dict: Dictionary with groups as keys and lists of numbers as values.
+    :param name_parameters: List of strings representing the categories for the bars in each group.
+    """
+    x = np.arange(len(name_parameters))# the label locations
+    x=x*0.35
+    print(x*0.3)
+    print(x)
+    
+    width = 0.08  # the width of the bars
+    multiplier = 0
+    hatches = ['/', 'x', 'o']  # List of patterns
+    colors=['red','green','blue']
+    other_colors=['none','none','blue']
+    fig, ax = plt.subplots(layout='constrained')
+
+    for i, (attribute, measurement) in enumerate(data_dict.items()):
+        offset = width * multiplier
+        # Apply a different hatch pattern to each group
+        print(x+offset)
+        hatch = hatches[i % len(hatches)]
+
+        rects = ax.bar(x + offset, measurement, width, edgecolor=colors[i],color=other_colors[i],lw=2.,label=attribute,hatch=hatch)
+        multiplier += 1
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Sysbench Score')
+    ax.set_xticks(x + width, name_parameters)
+    ax.legend(loc='upper left')
+    plt.show()
+# Initialize an empty list to hold the counts
+
+data = {
+    "CFS":[1,3],
+    "CFS+vProber":[4,5],
+    "CFS+Pinned":[2,3]
+}
+
+
+plot_grouped_data_with_legends(data,["NGNIX+Matmul","FIO+Matmul"])
+
--- SMT_inaware/parse-perf.py
+++ SMT_inaware/parse-perf.py
@@ -1,6 +1,7 @@
 import re
 import matplotlib.pyplot as plt
 import numpy as np
+import glob
 cpu_file = './tests/top_inaware_2_cpu09251738.txt'  # Replace with your actual file name
 
 io_file = './tests/top_inaware_2_io09251738.txt'  # Replace with your actual file name
@@ -16,6 +17,73 @@ penguin_means = {
     'CPU': [],
     'IO': [],
 }
+
+def get_average(list):
+    return sum(list)/len(list)
+
+def process_file(n,s):
+    files = glob.glob(s)
+    files.sort(reverse=True)
+
+    if len(files) >= n:
+        file_to_read = files[n-1]
+        cpu_sysbench_counts = []
+        with open(file_to_read, 'r') as f:
+            if "total number of events" in line:
+                match = re.search(r"total number of events:\s+(\d+)", line)
+                if match:
+                    print("Sysbench: ", match.group(1))
+                    amount_of_tests[cpu_increment // 2][(cpu_increment % 2)* 2 + 1] = match.group(1)
+                    species[cpu_increment] = species[cpu_increment] + "+Sysbench"
+                    penguin_means["CPU"].append(int(match.group(1)))
+                    cpu_increment+=1
+                    lines = f.readlines()
+            for line in lines:
+                if "total number of events" in line:
+                    cpu_amount = int(line.split()[-1])
+                    cpu_sysbench_counts.append(cpu_amount)
+                    print(cpu_amount)
+
+        return get_average(cpu_sysbench_counts)
+    else:
+        print(f"No matching file found for index {n}")
+        return []
+
+def plot_grouped_data_with_legends(data_dict, name_parameters):
+    """
+    Plots a grouped bar chart for each group in the data dictionary with 
+    separate bars for each category in name_parameters, using different patterns for each group.
+
+    :param data_dict: Dictionary with groups as keys and lists of numbers as values.
+    :param name_parameters: List of strings representing the categories for the bars in each group.
+    """
+    x = np.arange(len(name_parameters))# the label locations
+    x=x*0.35
+    print(x*0.3)
+    print(x)
+    
+    width = 0.08  # the width of the bars
+    multiplier = 0
+    hatches = ['/', 'x', 'o']  # List of patterns
+    colors=['red','green','blue']
+    other_colors=['none','none','blue']
+    fig, ax = plt.subplots(layout='constrained')
+
+    for i, (attribute, measurement) in enumerate(data_dict.items()):
+        offset = width * multiplier
+        # Apply a different hatch pattern to each group
+        print(x+offset)
+        hatch = hatches[i % len(hatches)]
+
+        rects = ax.bar(x + offset, measurement, width, edgecolor=colors[i],color=other_colors[i],lw=2.,label=attribute,hatch=hatch)
+        multiplier += 1
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Sysbench Score')
+    ax.set_xticks(x + width, name_parameters)
+    plt.ylim(20000, 37000)
+    ax.legend(loc='upper left',framealpha=1,prop = { "size": 10 },ncols=3)
+    plt.show()
 
 i = 0
 while i < len(cpulines):

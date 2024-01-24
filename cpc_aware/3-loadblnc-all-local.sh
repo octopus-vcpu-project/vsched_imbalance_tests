4869ac1baf9e6a59dcc7dd03349277c218071553
FarWrong <55039915+FarWrong@users.noreply.github.com>
2024-01-23 22:55:07 -0500

    Update 3-loadblnc-all-local.sh

--- cpc_aware/3-loadblnc-all-local.sh
+++ cpc_aware/3-loadblnc-all-local.sh
@@ -5,7 +5,8 @@ competed for VCPUS. Note that the linux scheduler actually does do a fine job of
 
 OUTPUT_FILE="./test/1-freq-unfair$(date +%m%d%H%M).txt"
 OUTPUT_FILE2="./test/2-freq-unfair$(date +%m%d%H%M).txt"
-
+OUTPUT_FILE3="./test/3-freq-unfair$(date +%m%d%H%M).txt"
+OUTPUT_FILE4="./test/4-freq-unfair$(date +%m%d%H%M).txt"
 prob_vm=$1
 compete_vm=$2
 
@@ -38,10 +39,15 @@ sleep 5
 ssh ubuntu@$prob_vm "sudo sysbench --threads=16 --time=20 cpu run"
 
 ssh ubuntu@$prob_vm "sudo $cpu_benchmark" >> "$OUTPUT_FILE" &
+ssh ubuntu@$prob_vm "sudo $cpu_benchmark" >> "$OUTPUT_FILE3" &
+ssh ubuntu@$prob_vm "sudo $cpu_benchmark" >> "$OUTPUT_FILE4" &
 ssh ubuntu@$prob_vm "sudo $cpu_benchmark" >> "$OUTPUT_FILE2" 
 ssh ubuntu@$prob_vm "sudo bash /home/ubuntu/cpu_profiler/setup_vcapacity.sh"
 ssh ubuntu@$prob_vm "nohup sudo /home/ubuntu/cpu_profiler/cpu_prober.out -v -i 5 -s 10000 &  " & 
 sleep 10 
+
+ssh ubuntu@$prob_vm "sudo $cpu_benchmark" >> "$OUTPUT_FILE3" &
+ssh ubuntu@$prob_vm "sudo $cpu_benchmark" >> "$OUTPUT_FILE4" &
 ssh ubuntu@$prob_vm "sudo $cpu_benchmark" >> "$OUTPUT_FILE" &
 ssh ubuntu@$prob_vm "sudo $cpu_benchmark" >> "$OUTPUT_FILE2" 
 sleep 10

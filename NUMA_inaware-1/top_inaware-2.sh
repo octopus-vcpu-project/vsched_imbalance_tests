97a08d55320fa532bd21d74ceb22c2f39e8bb4fb
FarWrong <55039915+FarWrong@users.noreply.github.com>
2024-01-24 13:15:39 -0500

    Update top_inaware-2.sh

--- NUMA_inaware-1/top_inaware-2.sh
+++ NUMA_inaware-1/top_inaware-2.sh
@@ -12,7 +12,7 @@ comm_benchmark="/var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1/wrk
 #comm_benchmark="/home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p dedup -n 16 -i native"
 cpu_benchmark="sysbench --threads=16 --time=10000 cpu run"
 virsh shutdown $prob_vm
-sudo bash ../utility/cleanon_startup.sh $prob_vm 3 2
+sudo bash ../utility/cleanon_startup.sh $prob_vm 32
 naive_topology_string="<cpu mode='custom' match='exact' check='none'>\n<model fallback='forbid'>qemu64</model>\n</cpu>"
 smart_topology_string="<cpu mode='custom' match='exact' check='none'>\n    <model fallback='forbid'>qemu64</model>\n    <topology sockets='2' dies='1' cores='16' threads='1'/></cpu>"
 comm_benchmark_1="/var/lib/phoronix-test-suite/installed-tests/pts/new-nginx-3/wrk-4.2.0/wrk -d 60s -c 300 -t 8 https://127.0.0.1:4054/test.html" 

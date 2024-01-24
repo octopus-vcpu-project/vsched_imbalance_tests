63614baaa6a299e644efe1ace4261d4a6279a8f8
FarWrong <55039915+FarWrong@users.noreply.github.com>
2024-01-23 20:28:58 -0500

    Update 3-loadblnc-all-local.sh

--- cpc_aware/3-loadblnc-all-local.sh
+++ cpc_aware/3-loadblnc-all-local.sh
@@ -33,7 +33,7 @@ done
 
 
 wipe_clean $prob_vm
-ssh ubuntu@$prob_vm "sudo $compete_benchmark" >> "$OUTPUT_FILE" &
+ssh ubuntu@$compete_benchmark "sudo $compete_benchmark" >> "$OUTPUT_FILE" &
 sleep 5
 ssh ubuntu@$prob_vm "sudo $cpu_benchmark" >> "$OUTPUT_FILE" &
 ssh ubuntu@$prob_vm "sudo $cpu_benchmark" >> "$OUTPUT_FILE2" 

c3e0fdb92a036ace9380a05b4b662070590db1de
FarWrong <55039915+FarWrong@users.noreply.github.com>
2024-01-24 12:19:42 -0500

    Update top_inaware-2.sh

--- NUMA_inaware-1/top_inaware-2.sh
+++ NUMA_inaware-1/top_inaware-2.sh
@@ -3,46 +3,20 @@ comm_benchmark="/var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1/wrk
 #comm_benchmark="/home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p dedup -n 16 -i native"
 cpu_benchmark="sysbench --threads=16 --time=10000 cpu run"
 virsh shutdown $prob_vm
-sudo bash ../utility/cleanon_startup.sh $prob_vm 20
+sudo bash ../utility/cleanon_startup.sh $prob_vm 32
 naive_topology_string="<cpu mode='custom' match='exact' check='none'>\n<model fallback='forbid'>qemu64</model>\n</cpu>"
 smart_topology_string="<cpu mode='custom' match='exact' check='none'>\n    <model fallback='forbid'>qemu64</model>\n    <topology sockets='1' dies='1' cores='20' threads='1'/></cpu>"
 #comm_benchmark_1="/var/lib/phoronix-test-suite/installed-tests/pts/new-nginx-3/wrk-4.2.0/wrk -d 60s -c 300 -t 8 https://127.0.0.1:8089/test.html" 
 comm_benchmark_1="sleep 65"
 
-toggle_topological_passthrough(){
-    virsh shutdown $prob_vm
-    while true; do
-        vm_state=$(virsh domstate "$prob_vm")
-        if [ "$vm_state" != "running" ]; then
-            echo "VM is shutdown"
-            break
-        else
-            echo "Waiting for VM to shutdown"
-            sleep 3 
-        fi
-    done
-    virsh dumpxml $prob_vm > /tmp/$prob_vm.xml
-    if [ $1 -eq 1 ]; then
-        sed -i "/<cpu /,/<\/cpu>/c\\$smart_topology_string" /tmp/$prob_vm.xml
-    else
-        sed -i "/<cpu /,/<\/cpu>/c\\$naive_topology_string" /tmp/$prob_vm.xml
-    fi
-    virsh define /tmp/$prob_vm.xml
-    sudo bash ../utility/cleanon_startup.sh $prob_vm 20
-    for i in {0..20};do
+for i in {0..15};do
         sudo virsh vcpupin $prob_vm $i $((i + 20))
-    done
+done
 
-    #for i in {16..31};do
-    #    sudo virsh vcpupin $prob_vm $i $((i + 24))
-    #done
-    echo "Pinning Complete"
-   ssh ubuntu@$prob_vm "sudo killall nginx"
-   ssh ubuntu@$prob_vm "cd /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1;sudo ./nginx_/sbin/nginx -g 'worker_processes auto;'"
-  # ssh ubuntu@$prob_vm "cd /var/lib/phoronix-test-suite/installed-tests/pts/new-nginx-3;sudo ./nginx_/sbin/nginx -g 'worker_processes 32;' -c /var/lib/phoronix-test-suite/installed-tests/pts/new-nginx-3/nginx_/conf/nginx.conf"
-  # sleep 10
-   ssh ubuntu@$prob_vm "sudo killall mysqld"
-} 
+for i in {16..31};do
+        sudo virsh vcpupin $prob_vm $i $((i + 40))
+done
+
 
 
 
@@ -85,7 +59,9 @@ echo "ipi test complete"
 #    ssh ubuntu@$prob_vm "sudo cat /sys/kernel/debug/sched/debug | grep -E 'cpu#|>R '" >> "$PLC_OUTPUT"
 #done
 wait
-toggle_topological_passthrough 1
+ssh ubuntu@$prob_vm "sudo insmod /home/ubuntu/vsched/custom_modules/cust_topo.ko" 
+ssh ubuntu@$prob_vm "sudo /home/ubuntu/vtop/a.out -f 1000" &
+
 echo "starting smart test suite"
 ssh ubuntu@$prob_vm "sudo $comm_benchmark" >> "$OUTPUT_FILE" &
 ssh ubuntu@$prob_vm "sudo $comm_benchmark_1" >> "$OUTPUT_FILE2" 

78f0024a4298083ffc212a7d80a55a8466b3aa4d
FarWrong <55039915+FarWrong@users.noreply.github.com>
2024-01-24 12:08:31 -0500

    Update parse-perf-simple.py

--- SMT_inaware/parse-perf-simple.py
+++ SMT_inaware/parse-perf-simple.py
@@ -3,55 +3,45 @@ import numpy as np
 
 # Data
 nginx_matmul3 = {
-    "Matmul": [100],
-    "Nginx": [100],
-    "Matmul+vProber": [113.89],
-    "Nginx+vProber": [104.927]
+    "Matmul": 100,
+    "Nginx": 100,
+    "Matmul+vProber": 113.89,
+    "Nginx+vProber": 104.927
 }
 
 nginx_matmul2 = {
-    "Matmul": [100],
-    "FIO": [100],
-    "Matmul+vProber": [118.26],
-    "FIO+vProber": [99.097]
+    "Matmul": 100,
+    "FIO": 100,
+    "Matmul+vProber": 118.26,
+    "FIO+vProber": 99.097
 }
 
-# Labels and values for nginx_matmul3
+# Prepare data for plotting
 labels_3 = list(nginx_matmul3.keys())
-values_3 = [nginx_matmul3[label][0] for label in labels_3]
-
-# Labels and values for nginx_matmul2
 labels_2 = list(nginx_matmul2.keys())
-values_2 = [nginx_matmul2[label][0] for label in labels_2]
+values_3 = list(nginx_matmul3.values())
+values_2 = list(nginx_matmul2.values())
 
-# Aligning the labels and ensuring both sets have the same labels
-all_labels = set(labels_3 + labels_2)
-values_3_aligned = [nginx_matmul3.get(label, [None])[0] for label in all_labels]
-values_2_aligned = [nginx_matmul2.get(label, [None])[0] for label in all_labels]
-
-# Number of groups
-n_groups = len(all_labels)
+# Plotting parameters
+bar_width = 0.35
+space_between_groups = 1.5
+index_3 = np.arange(len(labels_3))
+index_2 = np.arange(len(labels_2)) + max(index_3) + space_between_groups
 
 # Creating the plot
 fig, ax = plt.subplots()
 
-index = np.arange(n_groups)
-bar_width = 0.35
-opacity = 0.8
+# Plotting the bars for nginx_matmul3 and nginx_matmul2
+bars_3 = ax.bar(index_3, values_3, bar_width, label='nginx_matmul3')
+bars_2 = ax.bar(index_2, values_2, bar_width, label='nginx_matmul2')
 
-rects1 = ax.bar(index, values_3_aligned, bar_width,
-                alpha=opacity, color='b',
-                label='nginx_matmul3')
-
-rects2 = ax.bar(index + bar_width, values_2_aligned, bar_width,
-                alpha=opacity, color='g',
-                label='nginx_matmul2')
-
+# Adding labels and titles
 ax.set_xlabel('Category')
 ax.set_ylabel('Scores')
-ax.set_title('Scores by category and test')
-ax.set_xticks(index + bar_width / 2)
-ax.set_xticklabels(all_labels)
+ax.set_title('Scores by category for each dataset')
+ax.set_xticks(np.concatenate((index_3, index_2)))
+ax.set_xticklabels(labels_3 + labels_2)
 ax.legend()
 
+# Show the plot
 plt.show()

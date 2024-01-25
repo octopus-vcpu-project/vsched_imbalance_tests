aa5138c8f784af41b19a3c46d875c865a92201b8
FarWrong <55039915+FarWrong@users.noreply.github.com>
2024-01-25 03:21:53 -0500

    Create final_test.sh

---
+++ cpc_aware/final_test.sh
@@ -0,0 +1,105 @@
+bench_1_=("sysbench --threads=32 --time=60 cpu run")
+
+
+prob_vm="e-vm3"
+compete_vm_1="e-vm1"
+compete_vm_2="vsched-1"
+OUTPUT_FILE="./tests/output$(date +%m%d%H%M)naive.txt"
+OUTPUT_FILE_PROBE="./tests/output$(date +%m%d%H%M).txt"
+windup_compete_vms(){
+    sudo bash ../utility/cleanon_startup.sh $compete_vm_2 8
+    sudo bash ../utility/cleanon_startup.sh $compete_vm_1 32
+    for i in {0..15};do
+        sudo virsh vcpupin $compete_vm_1 $i $((i + 20))
+    done
+    for i in {16..31};do
+        sudo virsh vcpupin $compete_vm_1 $i $((i + 40 ))
+    done
+    for i in {0..7};do
+        sudo virsh vcpupin $compete_vm_2 $i $i
+    done
+    ssh ubuntu@$compete_vm_1 "sudo sysbench --threads=32 --time=10 cpu run"
+    ssh ubuntu@$compete_vm_2 "sudo sysbench --threads=32 --time=10 cpu run"
+}
+
+reset_prob_vm(){
+    virsh shutdown $prob_vm
+    sleep 10
+    sudo bash ../utility/cleanon_startup.sh $prob_vm 32
+    for i in {0..15};do
+        sudo virsh vcpupin $prob_vm $i $((i + 20))
+    done
+    for i in {16..31};do
+        sudo virsh vcpupin $prob_vm $i $((i + 40 ))
+    done
+    ssh ubuntu@$prob_vm "sudo sysbench --threads=32 --time=10 cpu run"
+}
+
+
+activate_vprobers(){
+    ssh ubuntu@$prob_vm "sudo insmod /home/ubuntu/vsched/custom_modules/cust_topo.ko" 
+    ssh ubuntu@$prob_vm "sudo /home/ubuntu/vtop/a.out -f 1000" &
+    ssh ubuntu@$prob_vm "sudo bash /home/ubuntu/cpu_profiler/setup_vcapacity.sh"
+    ssh ubuntu@$prob_vm "nohup sudo /home/ubuntu/cpu_profiler/cpu_prober.out -v -i 20 -s 10000 &  " & 
+    sleep 10
+}
+
+
+set_normal_mode(){
+    for i in {0..7};do
+        sudo virsh vcpupin $compete_vm_2 $i $i
+    done
+    for i in {0..15};do
+        sudo virsh vcpupin $prob_vm $i $((i + 20))
+    done
+    for i in {16..31};do
+        sudo virsh vcpupin $prob_vm $i $((i + 40 ))
+    done
+}
+
+set_interference_mode(){
+    for i in {0..15};do
+        sudo virsh vcpupin $prob_vm $i $(( (i%10) + 20))
+    done
+    for i in {16..31};do
+        sudo virsh vcpupin $prob_vm $i $((i + 40 ))
+    done
+
+    for i in {0..7};do
+        sudo virsh vcpupin $compete_vm_2 $i $((40))
+    done
+}
+
+
+windup_compete_vms
+reset_prob_vm
+length=${#bench_1_[@]}
+for ((i=0; i<length; i++)); do
+    bench_1=${bench_1_[$i]}
+    (
+        while true; do
+            set_normal_mode
+            sleep 5
+            set_interference_mode
+            sleep 5
+        done
+    ) &
+    mode_pid=$!
+    ssh ubuntu@$prob_vm "sudo $bench_1">>"${OUTPUT_FILE}_$i"
+done
+activate_vprobers
+ssh ubuntu@$prob_vm "sudo sysbench --threads=32 --time=10 cpu run"
+for ((i=0; i<length; i++)); do
+    bench_1=${bench_1_[$i]}
+    (
+        while true; do
+            set_normal_mode
+            sleep 5
+            set_interference_mode
+            sleep 5
+        done
+    ) &
+    mode_pid=$!
+    ssh ubuntu@$prob_vm "sudo $bench_1">>"${OUTPUT_FILE_PROBE}_$i" &
+    kill $mode_pid
+done

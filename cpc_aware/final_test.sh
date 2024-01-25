48a52bf0dce56d3d32f283b865a8b5d23802ed23
FarWrong <55039915+FarWrong@users.noreply.github.com>
2024-01-25 03:38:38 -0500

    asdf

--- cpc_aware/final_test.sh
+++ cpc_aware/final_test.sh
@@ -40,7 +40,7 @@ activate_vprobers(){
     ssh ubuntu@$prob_vm "sudo insmod /home/ubuntu/vsched/custom_modules/cust_topo.ko" 
     ssh ubuntu@$prob_vm "sudo /home/ubuntu/vtop/a.out -f 1000" &
     ssh ubuntu@$prob_vm "sudo bash /home/ubuntu/cpu_profiler/setup_vcapacity.sh"
-    ssh ubuntu@$prob_vm "nohup sudo /home/ubuntu/cpu_profiler/cpu_prober.out -v -i 20 -s 10000 &  " & 
+    ssh ubuntu@$prob_vm "nohup sudo /home/ubuntu/cpu_profiler/cpu_prober.out -v -i 20 -s 2000 &  " & 
     sleep 10
 }
 
@@ -79,9 +79,9 @@ for ((i=0; i<length; i++)); do
     (
         while true; do
             set_normal_mode
-            sleep 5
+            sleep 10
             set_interference_mode
-            sleep 5
+            sleep 10
         done
     ) &
     mode_pid=$!

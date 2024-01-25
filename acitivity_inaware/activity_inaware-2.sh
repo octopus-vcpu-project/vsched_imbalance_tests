191eac0c9dc91f7c6286bfd5ac7ecb9f53a47161
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2024-01-24 23:06:12 -0500

    Update activity_inaware-2.sh

--- acitivity_inaware/activity_inaware-2.sh
+++ acitivity_inaware/activity_inaware-2.sh
@@ -34,7 +34,7 @@ runLatencyTest(){
     latency_option=$1
     echo "Running Latency benchmark $1" 
     echo "Running Latency benchmark $1" >> "$OUTPUT_FILE" 
-    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$1;time sudo bash run.sh"  >> "$OUTPUT_FILE" 2>&1
+    ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/$1;time sudo bash run.sh" 
     ssh ubuntu@$prob_vm "cd /home/ubuntu/Workloads/Tailbench/tailbench/utilities;sudo python parselats-1.py ../$1/lats.bin"  >> "$OUTPUT_FILE" 2>&1
 }
 
@@ -48,38 +48,6 @@ setLatency(){
     echo "Set latency to $1" >> "$OUTPUT_FILE" 
 }
 
-toggle_topological_passthrough(){
-    naive_topology_string="<cpu mode='custom' match='exact' check='none'>\n<model fallback='forbid'>qemu64</model>\n</cpu>"
-    smart_topology_string="<cpu mode='custom' match='exact' check='none'>\n    <model fallback='forbid'>qemu64</model>\n    <topology sockets='2' dies='1' cores='16' threads='1'/></cpu>"
-    virsh shutdown $prob_vm
-    while true; do
-        vm_state=$(virsh domstate "$prob_vm")
-        if [ "$vm_state" != "running" ]; then
-            echo "VM is shutdown"
-            break
-        else
-            echo "Waiting for VM to shutdown"
-            sleep 3 
-        fi
-    done
-    virsh dumpxml $prob_vm > /tmp/$prob_vm.xml
-    if [ $1 -eq 1 ]; then
-        sed -i "/<cpu /,/<\/cpu>/c\\$smart_topology_string" /tmp/$prob_vm.xml
-    else
-        sed -i "/<cpu /,/<\/cpu>/c\\$naive_topology_string" /tmp/$prob_vm.xml
-    fi
-    virsh define /tmp/$prob_vm.xml
-    sudo bash ../utility/cleanon_startup.sh $prob_vm 32
-    for i in {0..15};do
-        sudo virsh vcpupin $prob_vm $i $((i + 20))
-    done
-
-    for i in {16..31};do
-        sudo virsh vcpupin $prob_vm $i $((i + 24))
-    done
-    echo "Pinning Complete"
-    ssh ubuntu@$prob_vm "sudo killall mysqld"
-} 
 
 runAllTests(){
     runLatencyTest "img-dnn"
@@ -95,11 +63,6 @@ runAllTests(){
 
 wake_and_pin_vm $compete_vm
 #Fetch VM PID and use that to fetch Cgroup title
-vm_pid=$(sudo grep pid /var/run/libvirt/qemu/$prob_vm.xml | awk -F "'" '{print $6}' | head -n 1)
-vm_cgroup_title=$(sudo cat /proc/$vm_pid/cgroup | awk -F "/" '{print $3}')
-
-c_vm_pid=$(sudo grep pid /var/run/libvirt/qemu/$compete_vm.xml | awk -F "'" '{print $6}' | head -n 1)
-c_vm_cgroup_title=$(sudo cat /proc/$c_vm_pid/cgroup | awk -F "/" '{print $3}')
 
 ssh ubuntu@$prob_vm "sudo killall a.out"
 ssh ubuntu@$compete_vm "sudo killall sysbench" 

3bb32a30f1bbcad87e5792dda3282dfac72a9459
FarWrong <55039915+FarWrong@users.noreply.github.com>
2024-01-26 15:10:37 -0800

    Update idle_vcpus-1.sh

--- idle_vcpus/idle_vcpus-1.sh
+++ idle_vcpus/idle_vcpus-1.sh
@@ -3,7 +3,7 @@ prob_vm=$1
 compete_vm=$2
 compete_vm_2=$3
 benchmark_time=20
-latency_bench="sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p canneal -n 32 -i native"
+latency_bench="sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p canneal -n 16 -i native"
 idler_bench="sudo bash /home/ubuntu/idle_load_generator/idler.sh"
 compete_bench="sudo sysbench --threads=32 --time=1000000 cpu run"
 get_lat_val="cd /home/ubuntu/Workloads/tailbench-v0.9/utilities;sudo python parselats-1.py ../img-dnn/lats.bin"

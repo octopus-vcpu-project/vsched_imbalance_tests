17aef0f93d3d7b2ca4ec0802d18a4ebecbca74b2
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2024-01-26 05:24:27 -0500

    Update final_test-16core-ver.sh

--- cpc_aware/final_test-16core-ver.sh
+++ cpc_aware/final_test-16core-ver.sh
@@ -217,7 +217,7 @@ for ((i=0; i<length; i++)); do
     ) &
    mode_pid=$!
     #ssh ubuntu@$prob_vm "sudo cset proc --set=benchmark_cpuset --exec $bench_1">>"${OUTPUT_FILE_PROBE}_$i"
-    ssh ubuntu@$prob_vm "$bench_1">>"${OUTPUT_FILE}_$i"
+    ssh ubuntu@$prob_vm "$bench_1">>"${OUTPUT_FILE_PROBE}_$i"
     sudo kill $mode_pid
 done
 getLatencyResultsSMRT

0543b69e21cc46ea90823889a13ef4dfe6129352
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2024-01-25 23:42:03 -0500

    Update final_test-16core-ver.sh

--- cpc_aware/final_test-16core-ver.sh
+++ cpc_aware/final_test-16core-ver.sh
@@ -3,7 +3,7 @@
 #bench_1_=("echo lol")
 #bench_1_=("sudo sysbench --threads=32 --time=40 cpu run")
 bench_1_=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p dedup -n 16 -i native")
-bench_1_+=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p streamcluster -n 16 -i native")
+#bench_1_+=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p streamcluster -n 16 -i native")
 #bench_1_+=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p bodytrack -n 16 -i native")
 #bench_1_+=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p facesim -n 16 -i native")
 #bench_1_+=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p canneal -n 16 -i native")

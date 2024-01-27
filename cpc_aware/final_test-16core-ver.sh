f890f816c806834c72d610ecb46d2fdb9e567013
edward <eguo3@pride.hofstra.edu>
2024-01-26 22:56:35 -0500

    new

--- cpc_aware/final_test-16core-ver.sh
+++ cpc_aware/final_test-16core-ver.sh
@@ -1,12 +1,12 @@
 
 
 #bench_1_=("echo lol")
-#bench_1_=("sudo sysbench -- --threads=32 --time=40 cpu run")
-#bench_1_=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -- -a run -p dedup -n 16 -i native")
+#bench_1_=("sudo sysbench -- --threads=8 --time=40 cpu run")
+#bench_1_=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -- -a run -p dedup -16 -i native")
 #bench_1_=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -- -a run -p streamcluster -n 16 -i native")
-#bench_1_+=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -- -a run -p bodytrack -n 16 -i native")
-#bench_1_=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -- -a run -p facesim -n 16 -i native")
-bench_1_=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -- -a run -p canneal -n 8 -i native")
+bench_1_=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -- -a run -p bodytrack -n 8 -i native")
+#bench_1_=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -- -a run -p facesim -n 8 -i native")
+#bench_1_=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -- -a run -p canneal -n 8 -i native")
 
 #bench_1_=("cd /home/ubuntu/Workloads/Tailbench/tailbench/img-dnn;sudo cset proc --set=benchmark_cpuset --exec bash run.sh")
 #bench_1_=("cd /home/ubuntu/Workloads/Tailbench/tailbench/moses;sudo cset proc --set=benchmark_cpuset --exec bash run.sh")

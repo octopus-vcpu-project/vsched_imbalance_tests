f3d1077818eba238929607b67e7c5d2d91a8fcc6
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2024-02-08 11:26:24 -0500

    Minor Cleanups

--- generate_steal_1.sh
+++
@@ -1,6 +0,0 @@
-VM_PATH="/sys/fs/cgroup/machine.slice/machine-qemu\x2d51\x2de\x2dvm1.scope/libvirt"
-run_slice=$1
-period=$2
-for i in {0..15};do
-	echo ${run_slice} ${period} > ${VM_PATH}/vcpu${i}/cpu.max
-done

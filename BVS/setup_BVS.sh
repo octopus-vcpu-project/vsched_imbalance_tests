715da4cfa3e5c424923bc7343c597b4b5f76fda2
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2024-04-29 11:59:05 -0400

    Create setup_BVS.sh

---
+++ BVS/setup_BVS.sh
@@ -0,0 +1,29 @@
+
+prob_vm=$1
+compete_vm=$2
+benchmark_command="sysbench --threads=16 --time=3 cpu run"
+
+sudo bash ../utility/cleanon_startup.sh $prob_vm 32
+sudo bash ../utility/cleanon_startup.sh $compete_vm 32
+
+#s
+for i in {0..31};do
+    sudo virsh vcpupin $prob_vm $i $((i))
+    sudo virsh vcpupin $compete_vm $i $((i))
+done
+
+
+setLatencyCFS(){
+    echo "Set latency cfs to $1" 
+    for i in {0..31};do
+        sudo echo $1 $2 > /sys/fs/cgroup/machine.slice/$vm_cgroup_title/libvirt/vcpu$i/cpu.max
+    done
+}
+
+
+
+OUTPUT_FILE="./data/BVStesting-$(date +%m%d%H%M).txt"
+
+
+setLatencyCFS 1000 2000
+setLatencyCFS 4000 8000

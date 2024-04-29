c82c3c6e6442c2b91e5df1ebec1d6a586277d9ad
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2024-04-29 12:04:23 -0400

    Update setup_BVS.sh

--- BVS/setup_BVS.sh
+++ BVS/setup_BVS.sh
@@ -5,6 +5,8 @@ benchmark_command="sysbench --threads=16 --time=3 cpu run"
 
 sudo bash ../utility/cleanon_startup.sh $prob_vm 32
 sudo bash ../utility/cleanon_startup.sh $compete_vm 32
+vm_pid=$(sudo grep pid /var/run/libvirt/qemu/$prob_vm.xml | awk -F "'" '{print $6}' | head -n 1)
+vm_cgroup_title=$(sudo cat /proc/$vm_pid/cgroup | awk -F "/" '{print $3}')
 
 #s
 for i in {0..31};do

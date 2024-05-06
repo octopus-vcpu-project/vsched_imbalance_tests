a2859ce9165df0c5061e77237e199cc10b1f9725
FarWrong <55039915+FarWrong@users.noreply.github.com>
2024-05-06 13:41:42 -0400

    Update ivh_parsec.sh

--- IVH/ivh_parsec.sh
+++ IVH/ivh_parsec.sh
@@ -1,66 +1,38 @@
 
 prob_vm=$1
 compete_vm=$2
-benchmark_command="sysbench --threads=16 --time=3 cpu run"
+compete_bench="sysbench --threads=16 --time=3 cpu run"
 
-sudo bash ../utility/cleanon_startup.sh $prob_vm 16
-sudo bash ../utility/cleanon_startup.sh $compete_vm 32
+sudo bash ../utility/cleanon_startup.sh $prob_vm 4
+sudo bash ../utility/cleanon_startup.sh $compete_vm 4
 
-bench_1_=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p dedup -n 16 -i native")
-bench_1_+=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p streamcluster -n 16 -i native")
-bench_1_+=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p bodytrack -n 16 -i native")
-bench_1_+=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p ferret -n 16 -i native")
-bench_1_+=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p canneal -n 16 -i native")
-bench_1_+=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p facesim -n 16 -i native")
-bench_1_+=("phoronix-test-suite batch-run imagemagick --batch-results")
-bench_1_+=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p canneal -n 16 -i native")
-bench_1_+=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p canneal -n 16 -i native")
-bench_1_+=("sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p canneal -n 16 -i native")
+vm_pid=$(sudo grep pid /var/run/libvirt/qemu/$prob_vm.xml | awk -F "'" '{print $6}' | head -n 1)
+vm_cgroup_title=$(sudo cat /proc/$vm_pid/cgroup | awk -F "/" '{print $3}')
+compete_vm_pid=$(sudo grep pid /var/run/libvirt/qemu/$compete_vm.xml | awk -F "'" '{print $6}' | head -n 1)
+compete_vm_cgroup_title=$(sudo cat /proc/$compete_vm_pid/cgroup | awk -F "/" '{print $3}')
 
-bench_1_+=("sudo /home/")
-
-
-#start_spot_vm(){
-
-
-#}
-
-for i in {0..15};do
+for i in {0..3};do
     sudo virsh vcpupin $prob_vm $i $((i))
     sudo virsh vcpupin $compete_vm $i $((i))
 done
 
-for i in {0..15};do
-	sudo virsh vcpupin $compete_vm $((i+16)) $((i%7))
-done
-
-
-activate_vprobers(){
-    ssh ubuntu@$prob_vm "sudo insmod /home/ubuntu/vsched/custom_modules/cust_topo.ko" 
-    #ssh ubuntu@$prob_vm "sudo /home/ubuntu/vtop/a.out -f 8 -s 4 -u 4000" &
-    ssh ubuntu@$prob_vm "sudo bash /home/ubuntu/runprober.sh"
-#    ssh ubuntu@$prob_vm "nohup sudo /home/ubuntu/cpu_profiler/a.out -i 20 -p 80 -s 10000 &  " & 
-    sleep 10
-}
+sudo echo 0 > /sys/kernel/debug/sched/wakeup_granularity_ns
 
 OUTPUT_FILE="./data/IVHtesting-$(date +%m%d%H%M).txt"
 
 length=${#bench_1_[@]}
 ssh ubuntu@$compete_vm "sudo sysbench --threads=16 --time=900000 cpu run &"&
 ssh ubuntu@$prob_vm "sudo killall a.out"
-activate_vprobers
 
-for ((i=0; i<length; i++)); do
-    bench_1=${bench_1_[$i]}
-    #without IVH
-    touch "${OUTPUT_FILE}_$i"
-    echo "Without IVH" >> "${OUTPUT_FILE}_$i"
-    
-    ssh ubuntu@$prob_vm "$bench_1">>"${OUTPUT_FILE}_$i"
+setLatency 5000 5000
 
-    ssh ubuntu@$prob_vm "sudo /home/ubuntu/vsched/tools/bpf/vcfs/atc" &
-    echo "With IVH" >> "${OUTPUT_FILE}_$i"
-    ssh ubuntu@$prob_vm "$bench_1">>"${OUTPUT_FILE}_$i"
-    ssh ubuntu@$prob_vm "sudo killall atc"
-    sleep 10 
-done
+setLatency(){
+    for i in $(seq 0 3);do
+        sudo echo $1 $(($1 * 2)) > /sys/fs/cgroup/machine.slice/$vm_cgroup_title/libvirt/vcpu$i/cpu.max
+    done
+    for i in $(seq 0 3);do
+        sudo echo $1 $(($1 * 2)) > /sys/fs/cgroup/machine.slice/$compete_vm_cgroup_title/libvirt/vcpu$i/cpu.max
+    done
+    echo "Set latency to $1" 
+    echo "Set latency to $1" >> "$OUTPUT_FILE" 
+}

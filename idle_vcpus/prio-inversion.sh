3144723d74a3a5cb07b0bb1f5a028a7a4c5f77da
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2024-05-06 15:29:08 -0400

    Update prio-inversion.sh

--- idle_vcpus/prio-inversion.sh
+++ idle_vcpus/prio-inversion.sh
@@ -3,11 +3,9 @@ prob_vm=$1
 compete_vm=$2
 benchmark_time=20
 swaption_test="sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p swaption -n 16 -i native"
-streamcluster_bench="sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p swaption -n 16 -i native"
+streamcluster_bench="sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p streamcluster -n 16 -i native"
 
 idler_bench="sudo bash /home/ubuntu/idle_load_generator/idler.sh"
-compete_bench="sudo sysbench --threads=32 --time=1000000 cpu run"
-get_lat_val="cd /home/ubuntu/Workloads/tailbench-v0.9/utilities;sudo python parselats-1.py ../img-dnn/lats.bin"
 OUTPUT_FILE="./data/prio_inversion-$(date +%m%d%H%M).txt"
 
 
@@ -23,7 +21,6 @@ wake_and_pin_prob(){
 
 
 
-vm_pid=$(sudo grep pid /var/run/libvirt/qemu/$prob_vm.xml | awk -F "'" '{print $6}' | head -n 1)
 
 sudo echo 3000000 > /sys/kernel/debug/sched/min_granularity_ns
 sudo echo 4000000 > /sys/kernel/debug/sched/wakeup_granularity_ns
@@ -33,7 +30,7 @@ ssh ubuntu@$prob_vm "sudo sysbench --time=10 --threads=16 cpu run"
 
 ssh ubuntu@$prob_vm "sudo sysbench --time=10 --threads=16 cpu run" 
 
-for i in {0..2};do
+for i in {0..0};do
    ssh ubuntu@$prob_vm "sudo killall a.out" 
    ssh ubuntu@$prob_vm "sudo $idler_bench" &
    echo "naive test" >> "$OUTPUT_FILE"
@@ -50,7 +47,7 @@ done
 
 sudo echo 1000000 > /sys/kernel/debug/sched/min_granularity_ns
 
-for i in {0..2};do
+for i in {0..0};do
    ssh ubuntu@$prob_vm "sudo killall a.out" 
    echo "non idle-just run" >> "$OUTPUT_FILE"
    ssh ubuntu@$prob_vm "sudo $swaption_test" >> "$OUTPUT_FILE" 

1da2b1570bb06e46a346e55298586ca18d8e6171
edward <eguo3@pride.hofstra.edu>
2024-05-13 22:16:31 -0400

    new

--- eval_test/bvs_init_test.sh
+++ eval_test/bvs_init_test.sh
@@ -81,13 +81,14 @@ setLatency(){
 sudo echo 1000 > /proc/sys/kernel/sched_cfs_bandwidth_slice_us
 activate_vprobers
 ssh ubuntu@$compete_vm "sudo $compete_bench" &
-setLatency 8000 16000 0 7
+setLatency 16000 32000 0 7
 setLatency 2000 4000 8 15
+sudo echo 18000000 > /sys/kernel/debug/sched/min_granularity_ns
+sudo echo 10000000 > /sys/kernel/debug/sched/wakeup_granularity_ns
+sudo echo 1000 > /proc/sys/kernel/sched_cfs_bandwidth_slice_us
 
 ssh ubuntu@$prob_vm "sudo killall a.out" 
 
-sudo echo 3000000 > /sys/kernel/debug/sched/min_granularity_ns
-sudo echo 4000000 > /sys/kernel/debug/sched/wakeup_granularity_ns
 sudo git add .;sudo git commit -m 'new';sudo git push
 
 

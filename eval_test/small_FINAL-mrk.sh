174d8f1a0ebfe48fc839abaf5f9b9dcf812c2aa5
edward <eguo3@pride.hofstra.edu>
2024-05-20 05:15:14 -0400

    new

--- eval_test/small_FINAL-mrk.sh
+++ eval_test/small_FINAL-mrk.sh
@@ -6,7 +6,7 @@ idler_bench="sudo bash /home/ubuntu/idle_load_generator/idler.sh"
 compete_bench="sudo sysbench --threads=32 --time=1000000 cpu run"
 OUTPUT_FILE="./data/obj-latency-noidle$(date +%m%d%H%M).txt"
 naive_topology_string="<cpu mode='custom' match='exact' check='none'>\n<model fallback='forbid'>qemu64</model>\n</cpu>"
-smart_topology_string="<cpu mode='custom' match='exact' check='none'>\n    <model fallback='forbid'>qemu64</model>\n    <topology sockets='1' dies='1' cores='16' threads='1'/></cpu>"
+smart_topology_string="<cpu mode='custom' match='exact' check='none'>\n    <model fallback='forbid'>qemu64</model>\n    <topology sockets='1' dies='1' cores='12' threads='1'/></cpu>"
 toggle_topological_passthrough(){
     virsh shutdown $prob_vm
     while true; do
@@ -30,16 +30,24 @@ toggle_topological_passthrough(){
 }
 
 toggle_topological_passthrough 0
-sudo bash ../utility/cleanon_startup.sh $prob_vm 16
+sudo bash ../utility/cleanon_startup.sh $prob_vm 12
 toggle_topological_passthrough 1
 
 
 wake_and_pin_vm(){
     select_vm=$1
-    sudo bash ../utility/cleanon_startup.sh $select_vm 16
+    sudo bash ../utility/cleanon_startup.sh $select_vm 12
     #set up first 8 cores 
-    for i in {0..15};do
-		virsh vcpupin $select_vm $((i)) $((i))
+    for i in {0..9};do
+    	if [ $((i % 2)) == 0 ]; then
+        	virsh vcpupin $select_vm $((i)) $((i))
+        elif [ $((i % 2)) == 1 ]; then
+        	virsh vcpupin $select_vm $((i)) $((i+79))
+        fi
+    done
+
+    for i in {10..11};do
+        virsh vcpupin $select_vm $((i)) $((10))
     done
     sleep 3
 }
@@ -49,6 +57,7 @@ sleep 5
 wake_and_pin_vm $prob_vm
 wake_and_pin_vm $compete_vm
 #THIS IS IMPORTANT - LAST CPU MUST BE SENT SOMEWHERE ELSE BECAUSE of stacking restrictions
+virsh vcpupin $compete_vm $((11)) $((20))
 
 vm_pid=$(sudo grep pid /var/run/libvirt/qemu/$prob_vm.xml | awk -F "'" '{print $6}' | head -n 1)
 vm_cgroup_title=$(sudo cat /proc/$vm_pid/cgroup | awk -F "/" '{print $3}')
@@ -69,13 +78,27 @@ setLatency(){
 }
 
 #Setting Next 3 cores  as High Latency/Low Capacity 
-setLatency 4000 8000 0 15 #6ms/18ms 
+setLatency 6000 18000 0 1 #6ms/18ms 
+
+#Setting Next 3 Cores as Low Latency/Low Capacity
+setLatency 2000 6000 2 3 #2ms/6ms 
+
+#Setting Next 3 Cores as Low Latency/Low Capacity
+setLatency 12000 18000 4 5 #12ms/18ms 
+
+#Setting Next 3 cores  as High Latency/High Capacity 
+setLatency 4000 6000 6 7 #4ms/6ms 
+
+#Two Straggler
+setLatency 1000 20000 8 9 #1ms/60ms 
+
+#Two Stacking
+setLatency 4000 6000 10 11 #1ms/60ms 
 
 
 
-
-sudo echo 4000000 > /sys/kernel/debug/sched/min_granularity_ns
-sudo echo 12000000 > /sys/kernel/debug/sched/wakeup_granularity_ns
+sudo echo 18000000 > /sys/kernel/debug/sched/min_granularity_ns
+sudo echo 10000000 > /sys/kernel/debug/sched/wakeup_granularity_ns
 sudo echo 1000 > /proc/sys/kernel/sched_cfs_bandwidth_slice_us
 ssh ubuntu@$prob_vm "sudo killall a.out"
 ssh ubuntu@$prob_vm "sudo tee /sys/kernel/debug/sched/min_granularity_ns <<< 1000000"
@@ -105,51 +128,15 @@ runPhoronixTest(){
 
 
 runPhoronixTests(){
-	runPhoronixTest "compress-pbzip2"
-	runPhoronixTest "build-imagemagick"
+	runPhoronixTest "x264"
 }
 
 runParsecTest(){
-    #local test_size="native"  # Default test size
-    #if [[ "$1" == "splash2x.ocean_ncp" ]]; then
-    #	test_size="simlarge"
-    #fi
-    #if [[ "$1" == "streamcluster" ]]; then
-     #   test_size="simlarge"
-    #fi
     for i in $(seq 1 1);do
         sleep 3
         echo "Running Parsec $1" 
-        echo "Running Parsec $1 set latency: 16" >> "$OUTPUT_FILE" 
-        ssh ubuntu@$prob_vm "sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p $1 -n 1 -i simlarge">>"$OUTPUT_FILE"
-	sleep 3
-    done
-    for i in $(seq 1 3);do
-        sleep 3
-        echo "Running Parsec $1" 
-        echo "Running Parsec $1 set latency: 8" >> "$OUTPUT_FILE" 
-        ssh ubuntu@$prob_vm "sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p $1 -n 2 -i simlarge">>"$OUTPUT_FILE"
-	sleep 3
-    done
-    for i in $(seq 1 3);do
-        sleep 3
-        echo "Running Parsec $1" 
-        echo "Running Parsec $1 set latency: 4" >> "$OUTPUT_FILE" 
-        ssh ubuntu@$prob_vm "sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p $1 -n 4 -i simlarge">>"$OUTPUT_FILE"
-	sleep 3
-    done
-    for i in $(seq 1 3);do
-        sleep 3
-        echo "Running Parsec $1" 
-        echo "Running Parsec $1 set latency: 2" >> "$OUTPUT_FILE" 
-        ssh ubuntu@$prob_vm "sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p $1 -n 8 -i simlarge">>"$OUTPUT_FILE"
-	sleep 3
-    done
-    for i in $(seq 1 3);do
-        sleep 3
-        echo "Running Parsec $1" 
-        echo "Running Parsec $1 set latency: 1" >> "$OUTPUT_FILE" 
-        ssh ubuntu@$prob_vm "sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p $1 -n 16 -i simlarge">>"$OUTPUT_FILE"
+        echo "Running Parsec $1" >> "$OUTPUT_FILE" 
+        ssh ubuntu@$prob_vm "sudo /home/ubuntu/Workloads/par-bench/bin/parsecmgmt -a run -p $1 -n 16 -i native">>"$OUTPUT_FILE"
     done
 }
 
@@ -168,56 +155,49 @@ runLatencyTests(){
 
 #parsec
 runParsecTests(){
-  #  runParsecTest "blackscholes"
-  #  runParsecTest "bodytrack"
- #   runParsecTest "canneal" 
-#    runParsecTest "dedup"
+#    runParsecTest "blackscholes"
+    runParsecTest "bodytrack"
+    runParsecTest "canneal" 
+    runParsecTest "dedup"
 #    runParsecTest "facesim" 
-#    runParsecTest "fluidanimate" 
- #   runParsecTest "freqmine"
+    runParsecTest "fluidanimate" 
+    runParsecTest "freqmine"
 #    runParsecTest "raytrace" 
-     runParsecTest "streamcluster"
- #   runParsecTest "swaptions" 
- #   runParsecTest "x624"
- #   runParsecTest "splash2x.barnes"
- #   runParsecTest "splash2x.fft"
-  #  runParsecTest "splash2x.lu_cb"
- #   runParsecTest "splash2x.lu_ncb"
-  #  runParsecTest "splash2x.ocean_cp"
-    runParsecTest "splash2x.ocean_ncp"
- #   runParsecTest "splash2x.radiosity"
- #   runParsecTest "splash2x.radix"
-   # runParsecTest "splash2x.raytrace"
-   # runParsecTest "splash2x.volrend"
-   # runParsecTest "splash2x.water_spatial"
+    runParsecTest "streamcluster"
+    runParsecTest "swaptions" 
+    runParsecTest "x624"
+    runParsecTest "splash2x.barnes"
+    runParsecTest "splash2x.fft"
+    runParsecTest "splash2x.lu_cb"
+    runParsecTest "splash2x.lu_ncb"
+    runParsecTest "splash2x.ocean_cp"
+ #   runParsecTest "splash2x.ocean_ncp"
+    runParsecTest "splash2x.radiosity"
+    runParsecTest "splash2x.radix"
+    runParsecTest "splash2x.raytrace"
+    runParsecTest "splash2x.volrend"
+    runParsecTest "splash2x.water_spatial"
 }
-
 activate_vprobers(){
     ssh ubuntu@$prob_vm "sudo insmod /home/ubuntu/vsched/custom_modules/cust_topo.ko" 
     ssh ubuntu@$prob_vm "sudo /home/ubuntu/vtop/a.out -f 60 -s 12 -u 8000" &
     ssh ubuntu@$prob_vm "sudo bash /home/ubuntu/runprober.sh"
     ssh ubuntu@$prob_vm 'sudo bash -c "echo "+cpuset" > /sys/fs/cgroup/cgroup.subtree_control"'
-    ssh ubuntu@$prob_vm "nohup sudo /home/ubuntu/cpu_profiler/cpu_prober.out -i 200000 -p 150 -s 10000 &  " & 
-    ssh ubuntu@$prob_vm "sudo /home/ubuntu/vsched/tools/bpf/vcfs/ivh" &
+    ssh ubuntu@$prob_vm "sudo /home/ubuntu/cpu_profiler/cpu_prober.out -i 200000 -p 150 -s 15000 &  " & 
     sleep 10
 }
+sleep 10
+runPhoronixTests
+
 activate_vprobers
 sleep 10
-#runLatencyTests
-#runPhoronixTests
-runParsecTests
-ssh ubuntu@$prob_vm "sudo killall ivh"
-sleep 5
-runParsecTests
-ssh ubuntu@$prob_vm "sudo /home/ubuntu/vsched/tools/bpf/vcfs/ivh-unaware" &
-sleep 10
-runParsecTest "streamcluster"
+runPhoronixTests
+ssh ubuntu@$prob_vm "sudo /home/ubuntu/vsched/tools/bpf/vcfs/vsched-small" &
+runPhoronixTests
 
-
-#sudo echo 3000000 > /sys/kernel/debug/sched/min_granularity_ns
-#sudo echo 4000000 > /sys/kernel/debug/sched/wakeup_granularity_ns
-#sudo echo 5000 > /proc/sys/kernel/sched_cfs_bandwidth_slice_us
+sudo echo 3000000 > /sys/kernel/debug/sched/min_granularity_ns
+sudo echo 4000000 > /sys/kernel/debug/sched/wakeup_granularity_ns
+sudo echo 5000 > /proc/sys/kernel/sched_cfs_bandwidth_slice_us
 sudo git add .;sudo git commit -m 'new';sudo git push
-
 
 

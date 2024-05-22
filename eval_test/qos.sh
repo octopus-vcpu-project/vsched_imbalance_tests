40434ecd96a10e35e4028ff184543642cd42f721
edward <eguo3@pride.hofstra.edu>
2024-05-22 05:28:04 -0400

    new

--- eval_test/qos.sh
+++ eval_test/qos.sh
@@ -95,7 +95,7 @@ makeSMT(){
 
 makeAssymetric(){
 	setLatency 6000 10000 0 3
-	setLatency 3500 10000 6 15
+	setLatency 3500 10000 4 15
 	outputToConsole
 }
 
@@ -125,8 +125,8 @@ activate_vprobers(){
 }
 #activate_vprobers
 sleep 10
-ssh ubuntu@$prob_vm "cd /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1;sudo ./nginx_/sbin/nginx -g 'worker_processes 1;'"
-ssh ubuntu@$prob_vm "sudo /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1/wrk-4.2.0/wrk -d 10000s -c 300 -t 7 https://127.0.0.1:8089/test.html -s /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1/new_script.lua" >> "$OUTPUT_FILE" &
+ssh ubuntu@$prob_vm "cd /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1;sudo ./nginx_/sbin/nginx -g 'worker_processes 4;'"
+ssh ubuntu@$prob_vm "sudo /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1/wrk-4.2.0/wrk -d 10000s -c 300 -t 4 https://127.0.0.1:8089/test.html -s /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1/new_script.lua" >> "$OUTPUT_FILE" &
 sleep 30
 makeContention
 sleep 30

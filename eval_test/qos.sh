418e6f9422585bfab6a4c40c6a960b7ae336dbc5
edward <eguo3@pride.hofstra.edu>
2024-05-22 01:40:55 -0400

    new

--- eval_test/qos.sh
+++ eval_test/qos.sh
@@ -126,8 +126,8 @@ activate_vprobers(){
     ssh ubuntu@$prob_vm "sudo /home/ubuntu/vsched/tools/bpf/vcfs/atc" &
     sleep 10
 }
-#activate_vprobers
-#sleep 10
+activate_vprobers
+sleep 10
 ssh ubuntu@$prob_vm "cd /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1;sudo ./nginx_/sbin/nginx -g 'worker_processes auto;'"
 ssh ubuntu@$prob_vm "sudo /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1/wrk-4.2.0/wrk -d 10000s -c 150 -t 2 https://127.0.0.1:8089/test.html -s /var/lib/phoronix-test-suite/installed-tests/pts/nginx-3.0.1/new_script.lua" >> "$OUTPUT_FILE" &
 sleep 30
